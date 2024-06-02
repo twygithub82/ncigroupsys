362686f65e5bc37040c941ffb4715a8de32bc311
Ang Boon Hong <boonhong@toppanleefung.com>
2024-06-02 14:23:14 +0800

    init version

---
+++ backend/graphql/Dot6.HotChoc12.CRUD.Demo/Data/Entities/Cake.cs
@@ -0,0 +1,11 @@
+namespace Dot6.HotChoc12.CRUD.Demo.Data.Entities;
+
+public class Cake
+{
+    public int Id{get;set;}
+    public string Name{get;set;}
+
+    public decimal Price {get; set;}
+
+    public string Description{get;set;}
+}
---
+++ backend/graphql/Dot6.HotChoc12.CRUD.Demo/GqlTypes/MutationType.cs
@@ -0,0 +1,37 @@
+using Dot6.HotChoc12.CRUD.Demo.Data;
+using Dot6.HotChoc12.CRUD.Demo.Data.Entities;
+using HotChocolate.Subscriptions;
+
+namespace Dot6.HotChoc12.CRUD.Demo.GqlTypes;
+
+public class MutationType
+{
+    public async Task<Cake> SaveCakeAsync([Service] MyWorldDBContext context, Cake newCake,[Service] ITopicEventSender topicEventSender)
+    {
+        context.Cake.Add(newCake);
+        await context.SaveChangesAsync();
+        await topicEventSender.SendAsync(nameof(SubscriptionType.CakeCreated),newCake);
+        return newCake;
+    }
+
+    public async Task<Cake>UpdateCakeAsync([Service] MyWorldDBContext context, Cake updateCake)
+    {
+        context.Cake.Update(updateCake);
+        await context.SaveChangesAsync();
+        return updateCake;
+    }
+
+
+    public async Task<string>DeleteCakeAsync([Service] MyWorldDBContext context, int id)
+    {
+       var cakeTodelete = await context.Cake.FindAsync(id);
+       if(cakeTodelete==null)
+       {
+         return "invalid operation";
+       }
+
+       context.Cake.Remove(cakeTodelete);
+       await context.SaveChangesAsync();
+       return "Deleted!";
+    }
+}
