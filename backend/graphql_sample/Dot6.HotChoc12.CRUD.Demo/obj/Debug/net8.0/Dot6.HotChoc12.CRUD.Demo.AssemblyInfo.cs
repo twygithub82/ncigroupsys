1ef8c1094c3e2687b11109c357fa545352015ec1
Wei Shun <weishun@toppanleefung.com>
2024-06-02 13:45:50 +0800

    commit wS

--- backend/graphql_sample/Dot6.HotChoc12.CRUD.Demo/obj/Debug/net8.0/Dot6.HotChoc12.CRUD.Demo.AssemblyInfo.cs
+++ backend/graphql_sample/Dot6.HotChoc12.CRUD.Demo/obj/Debug/net8.0/Dot6.HotChoc12.CRUD.Demo.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Dot6.HotChoc12.CRUD.Demo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6041e41726bd68a177027993713c11c9b413c318")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e28646a65aeeaee174c207c87d93a7685a3da54b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Dot6.HotChoc12.CRUD.Demo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Dot6.HotChoc12.CRUD.Demo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
