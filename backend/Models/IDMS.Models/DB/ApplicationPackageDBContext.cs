668c9f4e4f4b77d22b1c865a0d4835a7626627c8
Boon Hong ANG <boonhong@toppannext.com>
2024-07-22 08:37:16 +0800

    create package cleaning be and update the gateway configuration for package cleaning

---
+++ backend/Models/IDMS.Models/DB/ApplicationPackageDBContext.cs
@@ -0,0 +1,30 @@
+﻿using IDMS.Models.Inventory.InGate.GqlTypes.DB;
+using IDMS.Models.Master;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IDMS.Models.DB
+{
+    public class ApplicationPackageDBContext: DbContext
+    {
+        public ApplicationPackageDBContext(DbContextOptions<ApplicationPackageDBContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+
+
+        public DbSet<customer_company> customer_company { get; set; }
+        public DbSet<IDMS.Models.Parameter.cleaning_category> cleaning_category { get; set; }
+        public DbSet<IDMS.Models.Package.customer_company_cleaning_category_with_customer_company> customer_company_cleaning_category { get; set; }
+    }
+
+        
+}
