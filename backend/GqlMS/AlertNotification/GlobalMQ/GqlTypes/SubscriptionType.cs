c97dffa58760dc0d3f1117d82e6c303546609847
Boon Hong ANG <boonhong@toppannext.com>
2024-07-28 14:39:45 +0800

    notification init

---
+++ backend/GqlMS/AlertNotification/GlobalMQ/GqlTypes/SubscriptionType.cs
@@ -0,0 +1,10 @@
+﻿using IDMS.Models;
+
+namespace GlobalMQ.GqlTypes
+{
+    public class SubscriptionType
+    {
+        [Subscribe]
+        public Message MessageReceived([EventMessage] Message message) => message;
+    }
+}
