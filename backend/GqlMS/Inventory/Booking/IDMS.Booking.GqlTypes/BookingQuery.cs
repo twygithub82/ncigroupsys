0a45ad81f2c810008480691ec6ae51912e313508
Wymund <weaiyep2002@gmail.com>
2024-07-29 22:36:23 +0800

    added booking into gateway

--- backend/GqlMS/Inventory/Booking/IDMS.Booking.GqlTypes/BookingQuery.cs
+++ backend/GqlMS/Inventory/Booking/IDMS.Booking.GqlTypes/BookingQuery.cs
@@ -19,15 +19,15 @@ namespace IDMS.Booking.GqlTypes
         {
             try
             {
-                //var bookingDetail = context.booking.Where(d => d.delete_dt == null || d.delete_dt == 0)
-                //    .Include(b => b.storing_order_tank)
-                //        .ThenInclude(s => s.tariff_cleaning)
-                //    .Include(b => b.storing_order_tank)
-                //        .ThenInclude(s => s.storing_order)
-                //            .ThenInclude(c => c.customer_company);
+                var bookingDetail = context.booking.Where(d => d.delete_dt == null || d.delete_dt == 0)
+                    .Include(b => b.storing_order_tank)
+                        .ThenInclude(s => s.tariff_cleaning)
+                    .Include(b => b.storing_order_tank)
+                        .ThenInclude(s => s.storing_order)
+                            .ThenInclude(c => c.customer_company);
 
-                var bookingDetail = context.booking.Where(b => b.delete_dt == null || b.delete_dt == 0)
-                    .Include(b => b.storing_order_tank);
+                //var bookingDetail = context.booking.Where(b => b.delete_dt == null || b.delete_dt == 0)
+                //    .Include(b => b.storing_order_tank);
 
                 return bookingDetail;
 
