5c13b5f9a1779610f4c101b1068442d4493c7b01
Wymund <weaiyep2002@gmail.com>
2024-08-22 20:20:35 +0800

    added out-gate-implementation

--- backend/GqlMS/Inventory/InGateSurvey - V2/IDMS.InGateSurvey.GqlTypes/Query.cs
+++ backend/GqlMS/Inventory/InGateSurvey - V2/IDMS.InGateSurvey.GqlTypes/Query.cs
@@ -1,6 +1,9 @@
-﻿using IDMS.Models.Inventory.InGate.GqlTypes.DB;
+﻿using AutoMapper;
+using IDMS.InGateSurvey.Model.Request;
+using IDMS.Models.Inventory.InGate.GqlTypes.DB;
 using IDMS.Models.Shared;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,6 +14,22 @@ namespace IDMS.InGateSurvey.GqlTypes
 {
     public class Query
     {
-     
+        //public async Task<Record> AddTestSur(ApplicationInventoryDBContext context)
+        //{
+        //    int res = 3;
+        //    List<string> list = new List<string>() {"assds2142ascsdfa", "dshdgajs234234jhsdkas", "3423ksdj9asaalf" };
+
+
+        //    Record record = new Record()
+        //    {
+        //        affected = res,
+        //        guid = list
+        //    };
+
+
+        //    return record;
+
+        //}
     }
+
 }
--- backend/GqlMS/Inventory/InGateSurvey - V2/IDMS.InGateSurvey.Model/StatusConstant.cs
+++ backend/GqlMS/Inventory/InGateSurvey - V2/IDMS.InGateSurvey.Model/StatusConstant.cs
@@ -13,4 +13,27 @@ namespace IDMS.InGateSurvey.Model
         public const string PUBLISHED = "PUBLISHED";
         public const string CANCELED = "CANCELED";
     }
+
+    public static class TankMovementStatus
+    {
+        public const string CLEANING = "CLEANING";
+        public const string INGATE = "IN_GATE";
+        public const string INGATE_SURVEY = "IN_SURVEY";
+        public const string OUTGATE = "OUT_GATE";
+        public const string OUTGATE_SURVEY = "OUT_SURVEY";
+        public const string RESIDUE = "RESIDUE";
+        public const string REPAIR = "REPAIR";
+        public const string STEAM = "STEAM";
+        public const string STORAGE = "STORAGE";
+        public const string RO = "RO_GENERATED";
+    }
+
+    public static class ROStatus
+    {
+        public const string CANCELED = "CANCELED";
+        public const string DELETED = "DELETED";
+        public const string PENDING = "PENDING";
+        public const string PROCESSING = "PROCESSING";
+        public const string COMPLETED = "COMPLETED";
+    }
 }
