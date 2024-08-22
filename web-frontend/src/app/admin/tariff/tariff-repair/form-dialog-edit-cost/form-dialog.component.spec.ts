2e8ecb294f800b4d2631c8f899fa103543e7b8e0
Boon Hong ANG <boonhong@toppannext.com>
2024-08-22 22:28:28 +0800

    update tariff repair fe and be

--- web-frontend/src/app/admin/tariff/tariff-repair/form-dialog-edit-cost/form-dialog.component.spec.ts
+++ web-frontend/src/app/admin/tariff/tariff-repair/form-dialog-edit-cost/form-dialog.component.spec.ts
@@ -1,20 +1,20 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { FormDialogComponent_View } from './form-dialog.component';
+import { FormDialogComponent_Edit_Cost } from './form-dialog.component';
 
 describe('FormDialogComponent', () => {
-  let component: FormDialogComponent_View;
-  let fixture: ComponentFixture<FormDialogComponent_View>;
+  let component: FormDialogComponent_Edit_Cost;
+  let fixture: ComponentFixture<FormDialogComponent_Edit_Cost>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-    imports: [FormDialogComponent_View]
+    imports: [FormDialogComponent_Edit_Cost]
 })
     .compileComponents();
   });
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(FormDialogComponent_View);
+    fixture = TestBed.createComponent(FormDialogComponent_Edit_Cost);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
