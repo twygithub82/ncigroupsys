4b270d10f562a22a26bd6d5e54d485a221cb1864
Wymund <weaiyep2002@gmail.com>
2024-09-08 20:18:13 +0800

    refine select distinct length with unit

--- backend/Models/IDMS.Models/Result.cs
+++ backend/Models/IDMS.Models/Result.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,5 +10,11 @@ namespace IDMS.Models
     public class Result
     {
         public int result { get; set; } = -1;
+    }
+
+    public class LengthWithUnit
+    {
+        public double? length { get; set; }
+        public string? length_unit_cv { get; set; }
     }
 }
