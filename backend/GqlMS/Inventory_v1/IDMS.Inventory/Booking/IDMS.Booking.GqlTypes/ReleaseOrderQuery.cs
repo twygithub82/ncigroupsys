adc1891f1e52279b8e2b8c6236000f442884e3d6
Wymund <weaiyep2002@gmail.com>
2024-09-14 21:50:52 +0800

    merge SO,Booking,InOutGate into Inventory_V1 solution project

---
+++ backend/GqlMS/Inventory_v1/IDMS.Inventory/Booking/IDMS.Booking.GqlTypes/ReleaseOrderQuery.cs
@@ -0,0 +1,34 @@
+﻿using HotChocolate;
+using Microsoft.EntityFrameworkCore;
+using HotChocolate.Types;
+using Microsoft.AspNetCore.Http;
+using IDMS.Models.Inventory.InGate.GqlTypes.DB;
+using IDMS.Models;
+using IDMS.Inventory.GqlTypes;
+
+namespace IDMS.Booking.GqlTypes
+{
+    [ExtendObjectType(typeof(Query))]
+    public class ReleaseOrderQuery
+    {
+        [UsePaging(IncludeTotalCount = true, DefaultPageSize = 10)]
+        [UseProjection]
+        [UseFiltering]
+        [UseSorting]
+        public IQueryable<release_order> QueryReleaseOrder(ApplicationInventoryDBContext context, [Service] IHttpContextAccessor httpContextAccessor)
+        {
+            try
+            {
+                var roDetails = context.release_order.Where(d => d.delete_dt == null || d.delete_dt == 0)
+                    //.Include(d => d.scheduling.Where(s => s.delete_dt == null || s.delete_dt == 0))
+                    .Include(d => d.customer_company);
+
+                return roDetails;
+            }
+            catch (Exception ex)
+            {
+                throw new GraphQLException(new Error($"{ex.Message}--{ex.InnerException}", "ERROR"));
+            }
+        }
+    }
+}
---
+++ backend/GqlMS/Inventory_v1/IDMS.Inventory/InGate/IDMS.InGate.GqlTypes/OutGate_Query.cs
@@ -0,0 +1,63 @@
+﻿using HotChocolate.Authorization;
+using IDMS.Models;
+using Microsoft.AspNetCore.Http;
+using System.Security.Claims;
+
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+using Microsoft.AspNetCore.Identity;
+using IDMS.Models.Inventory;
+using Microsoft.EntityFrameworkCore;
+using IDMS.Models.Inventory.InGate.GqlTypes.DB;
+using IDMS.InGateSurvey.GqlTypes;
+
+namespace IDMS.InGate.GqlTypes
+{
+    [ExtendObjectType(typeof(Query))]
+    public class OutGate_Query
+    {
+        // [Authorize]
+        [UsePaging(IncludeTotalCount = true, DefaultPageSize = 10)]
+        // [UseProjection]
+        [UseFiltering]
+        [UseSorting]
+        public IQueryable<out_gate> QueryOutGates(ApplicationInventoryDBContext context, [Service] IConfiguration config, [Service] IHttpContextAccessor httpContextAccessor)
+        {
+            IQueryable<out_gate> query = null;
+            try
+            {
+
+                GqlUtils.IsAuthorize(config, httpContextAccessor);
+                query = context.out_gate.Where(i => i.delete_dt == null || i.delete_dt == 0)
+                    .Include(s => s.tank).Where(i => i.tank != null).Where(i => i.tank.delete_dt == null || i.tank.delete_dt == 0)
+                    .Include(s => s.tank.storing_order)
+                        .ThenInclude(c => c.customer_company)
+                    .Include(s => s.tank.tariff_cleaning)
+                    .Include(s => s.tank.tariff_cleaning.cleaning_method)
+                    .Include(s => s.tank.tariff_cleaning.cleaning_category)
+                    .Include(s => s.tank.release_order_sot)
+                        .ThenInclude(r => r.release_order);
+                //.Include(s => s.in_gate_survey);
+                // .Include(s=>s.tank.tariff_cleaning.cleaning_method);
+                foreach (var q in query)
+                {
+                    if (q.tank != null)
+                        if (q.tank.release_order_sot != null)
+                        {
+                            var ro = q.tank.release_order_sot.Where(s => s.sot_guid == q.tank.guid && (s.delete_dt == null || s.delete_dt == 0))
+                                .FirstOrDefault()?.release_order;
+                            if (ro != null)
+                                q.haulier = ro.haulier;
+                        }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+                throw new GraphQLException(new Error($"{ex.Message} -- {ex.InnerException}", "ERROR"));
+            }
+
+            return query;
+        }
+    }
+}
