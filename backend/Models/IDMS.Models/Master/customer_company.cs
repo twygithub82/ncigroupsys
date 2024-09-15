893cdc30d72d3967f3becddd76f5d7ed25e4bbbb
Wymund <weaiyep2002@gmail.com>
2024-09-15 12:36:34 +0800

    refactor customer_company class

--- backend/Models/IDMS.Models/Master/customer_company.cs
+++ backend/Models/IDMS.Models/Master/customer_company.cs
@@ -1,9 +1,12 @@
 ﻿using IDMS.Models.Inventory;
 using IDMS.Models.Package;
+using IDMS.Models.Shared;
+
 //using IDMS.StoringOrder.Model.Domain.Customer;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace IDMS.Models.Master
 {
@@ -11,25 +14,28 @@ namespace IDMS.Models.Master
     {
         [Key]
         public string? guid { get; set; }
-        public string name { get; set; }
-        public string code { get; set; }
-        public string description { get; set; }
-        public string alias { get; set; }
-        public string tariff_depot_guid { get; set; }
-        public string address_line1 { get; set; }
-        public string address_line2 { get; set; }
-        public string city { get; set; }
-        public string country { get; set; }
-        public string postal { get; set; }
-        public string phone { get; set; }
-        public string fax { get; set; }
-        public string email { get; set; }
-        public string website { get; set; }
 
-        public long effective_dt { get; set; }
-        public long agreement_due_dt { get; set; }
+        [ForeignKey("currency")]
+        public string? currency_guid { get; set; }
+        public string? name { get; set; }
         public string? type_cv { get; set; }
-        public string? currency_cv { get; set; }
+        public string? code { get; set; }
+        public string? description { get; set; }
+        public string? alias { get; set; }
+        public string? address_line1 { get; set; }
+        public string? address_line2 { get; set; }
+        public string? city { get; set; }
+        public string? country { get; set; }
+        public string? postal { get; set; }
+        public string? phone { get; set; }
+        public string? fax { get; set; }
+        public string? email { get; set; }
+        public string? website { get; set; }
+        public long? effective_dt { get; set; }
+        public long? agreement_due_dt { get; set; }
+        //public string? tariff_depot_guid { get; set; }
+
+        public currency? currency { get; set; }
 
         public IEnumerable<storing_order>? storing_orders { get; set; }
         public IEnumerable<customer_company_contact_person>? cc_contact_person { get; set; }
