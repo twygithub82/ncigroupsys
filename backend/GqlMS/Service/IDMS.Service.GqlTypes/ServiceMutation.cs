b335f0e7ec69c5126df15c21e7e6b59280ba4916
Wymund <weaiyep2002@gmail.com>
2024-10-26 22:36:16 +0800

    add job order mutation completed

--- backend/GqlMS/Service/IDMS.Service.GqlTypes/ServiceMutation.cs
+++ backend/GqlMS/Service/IDMS.Service.GqlTypes/ServiceMutation.cs
@@ -1,13 +1,53 @@
-﻿using System;
+﻿using HotChocolate;
+using IDMS.Models.Service.GqlTypes.DB;
+using IDMS.Models.Service;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CommonUtil.Core.Service;
 
 namespace IDMS.Service.GqlTypes
 {
     public class ServiceMutation
     {
+        public async Task<int> AddJobOrder(ApplicationServiceDBContext context, [Service] IHttpContextAccessor httpContextAccessor,
+         [Service] IConfiguration config, job_order jobOrder)
+        {
+            try
+            {
+                if (jobOrder == null)
+                    throw new GraphQLException(new Error($"Job order object cannot be null", "ERROR"));
+
+                if (jobOrder.working_hour == null)
+                    throw new GraphQLException(new Error($"Working hour cannot be null or empty", "ERROR"));
+
+                if (jobOrder.total_hour == null)
+                    throw new GraphQLException(new Error($"Total hour cannot be null or empty", "ERROR"));
+
+
+                var user = GqlUtils.IsAuthorize(config, httpContextAccessor);
+                long currentDateTime = DateTime.Now.ToEpochTime();
+
+                jobOrder.guid = Util.GenerateGUID();
+                jobOrder.create_by = user;
+                jobOrder.create_dt = currentDateTime;
+                jobOrder.status_cv = CurrentServiceStatus.PENDING;
+                await context.job_order.AddAsync(jobOrder);
+
+                var res = await context.SaveChangesAsync();
+                //TODO
+                //await topicEventSender.SendAsync(nameof(Subscription.CourseCreated), course);
+                return res;
+            }
+            catch (Exception ex)
+            {
+                throw new GraphQLException(new Error($"{ex.Message}--{ex.InnerException}", "ERROR"));
+            }
+        }
 
     }
 }
