e1ddca19df7d4245046e687de0c9f674a40685c9
Wymund <weaiyep2002@gmail.com>
2024-10-26 20:49:46 +0800

    modify team table fields

---
+++ backend/Models/IDMS.Models/Shared/team.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IDMS.Models.Shared
+{
+    public class team: Dates
+    {
+        [Key]
+        public string? guid {  get; set; }
+        public string? description { get; set; }
+        public string? department_cv { get; set; }
+    }
+}
