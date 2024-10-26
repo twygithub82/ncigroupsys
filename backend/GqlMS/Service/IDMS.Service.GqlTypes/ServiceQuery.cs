06fd47d05b1546bf433d5cb7fea112c27e784dda
Wymund <weaiyep2002@gmail.com>
2024-10-26 21:46:35 +0800

    added shared service mutation and query

---
+++ backend/GqlMS/Service/IDMS.Service.GqlTypes/ServiceQuery.cs
@@ -0,0 +1,69 @@
+﻿using HotChocolate.Types;
+using HotChocolate;
+using Microsoft.AspNetCore.Http;
+using IDMS.Models.Service.GqlTypes.DB;
+using IDMS.Models.Shared;
+using Microsoft.EntityFrameworkCore;
+using IDMS.Models.Service;
+
+namespace IDMS.Service.GqlTypes
+{
+    public class ServiceQuery
+    {
+        [UsePaging(IncludeTotalCount = true, DefaultPageSize = 10)]
+        [UseProjection]
+        [UseFiltering]
+        [UseSorting]
+        public IQueryable<job_order> QueryJobOrder(ApplicationServiceDBContext context, [Service] IHttpContextAccessor httpContextAccessor)
+        {
+            try
+            {
+                var jobOrders = context.job_order
+                                .Include(j => j.storing_order_tank)
+                                .Include(j => j.team)
+                                .Where(d => d.delete_dt == null || d.delete_dt == 0);
+
+                return jobOrders;
+            }
+            catch (Exception ex)
+            {
+                throw new GraphQLException(new Error($"{ex.Message}--{ex.InnerException}", "ERROR"));
+            }
+        }
+
+
+        [UsePaging(IncludeTotalCount = true, DefaultPageSize = 10)]
+        [UseProjection]
+        [UseFiltering]
+        [UseSorting]
+        public IQueryable<team> QueryTeams(ApplicationServiceDBContext context, [Service] IHttpContextAccessor httpContextAccessor)
+        {
+            try
+            {
+                var teamDetails = context.team.Where(d => d.delete_dt == null || d.delete_dt == 0);
+                return teamDetails;
+            }
+            catch (Exception ex)
+            {
+                throw new GraphQLException(new Error($"{ex.Message}--{ex.InnerException}", "ERROR"));
+            }
+        }
+
+        [UsePaging(IncludeTotalCount = true, DefaultPageSize = 10)]
+        [UseProjection]
+        [UseFiltering]
+        [UseSorting]
+        public IQueryable<aspnetusers> QueryUsers(ApplicationServiceDBContext context, [Service] IHttpContextAccessor httpContextAccessor)
+        {
+            try
+            {
+                var user = context.aspnetusers.Include(a => a.aspnetuserroles);
+                return user;
+            }
+            catch (Exception ex)
+            {
+                throw new GraphQLException(new Error($"{ex.Message}--{ex.InnerException}", "ERROR"));
+            }
+        }
+    }
+}
---
+++ backend/GqlMS/Service/IDMS.Service.GqlTypes/StatusConstant.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IDMS.Service.GqlTypes
+{
+    //public class StatusConstant
+    //{
+        public static class CurrentServiceStatus
+        {
+            public const string PENDING = "PENDING";
+            public const string APPROVE = "APPROVE";
+            public const string IN_PROGRESS = "JOB_IN_PROGRESS";
+            public const string QC = "QC_COMPLETED";
+            public const string BILL = "BILL_COMPLETED";
+            public const string CANCEL = "CANCELED";
+            public const string NO_ACTION = "NO_ACTION";
+            public const string KIV = "KIV";
+        }
+
+
+        public static class ObjectAction
+        {
+            public const string NEW = "NEW";
+            public const string EDIT = "EDIT";
+            public const string ROLLBACK = "ROLLBACK";
+            public const string CANCEL = "CANCEL";
+            public const string KIV = "KIV";
+            public const string APPROVE = "APPROVE";
+            public const string NA = "NA";
+        }
+ //   }
+
+    //public enum CodeTyp
+    //{
+    //    damage = 0,
+    //    repair = 1
+    //}
+}
