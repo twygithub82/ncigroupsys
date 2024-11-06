04aff8ea2856d5d289bbb22ae443987d509e1d34
Wymund <weaiyep2002@gmail.com>
2024-11-06 21:33:01 +0800

    added jobNotification model

---
+++ backend/Models/IDMS.Models/Notification/JobNotification.cs
@@ -0,0 +1,17 @@
+﻿using IDMS.Models.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IDMS.Models.Notification
+{
+    public class JobNotification
+    { 
+        public string job_status { get; set; }
+        public string job_order_guid { get; set; }
+        public long? start_time { get; set; }
+        public long? stop_time { get; set; }    
+    }
+}
