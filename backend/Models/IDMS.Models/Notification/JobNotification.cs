97d5c5af0cf17292bc099a35305b7a80189f04d3
Wymund <weaiyep2002@gmail.com>
2024-11-07 23:04:47 +0800

    add addtional field in JobNotification Model

--- backend/Models/IDMS.Models/Notification/JobNotification.cs
+++ backend/Models/IDMS.Models/Notification/JobNotification.cs
@@ -9,9 +9,13 @@ namespace IDMS.Models.Notification
 {
     public class JobNotification
     { 
-        public string job_status { get; set; }
-        public string job_order_guid { get; set; }
+        public string? time_table_guid { get; set; }
+        public string? job_order_guid { get; set; }
+        public string? item_guid { get; set; }
+        public string? job_status { get; set; }
+        public string? job_type { get; set; }
         public long? start_time { get; set; }
         public long? stop_time { get; set; }    
+        public long? complete_dt { get; set; }
     }
 }
