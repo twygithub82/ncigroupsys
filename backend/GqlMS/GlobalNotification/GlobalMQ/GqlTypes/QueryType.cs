259050c007684b82d9837568c2142a31633af11b
Wymund <weaiyep2002@gmail.com>
2024-11-07 23:05:28 +0800

    add JobNotification Subcription

--- backend/GqlMS/GlobalNotification/GlobalMQ/GqlTypes/QueryType.cs
+++ backend/GqlMS/GlobalNotification/GlobalMQ/GqlTypes/QueryType.cs
@@ -1,10 +1,12 @@
-﻿using HotChocolate.Data;
+﻿using GlobalMQ.LocalModel;
+using HotChocolate.Data;
 using HotChocolate.Subscriptions;
 using IDMS.Models;
 using IDMS.Models.DB;
 using IDMS.Models.GqlTypes;
 using IDMS.Models.Notification;
 using IDMS.Models.Package;
+using System;
 
 namespace GlobalMQ.GqlTypes
 {
@@ -17,6 +19,45 @@ namespace GlobalMQ.GqlTypes
             try
             {
                 await topicEventSender.SendAsync(nameof(SubscriptionType.MessageReceived), message);
+                return value;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public async Task<string> SendJobNotification(JobNotification jobNotification, int type, [Service] ITopicEventSender topicEventSender)
+        {
+            string value = "ok";
+            try
+            {
+                string prefix = "On";
+                string methodName = "";
+                string topicName = "";
+
+                if (type == JobNotificationType.START_JOB)
+                {
+                    methodName = nameof(SubscriptionType.JobStarted);
+                    topicName = $"{prefix}{methodName}_{jobNotification.job_order_guid}";
+                }
+                else if (type == JobNotificationType.STOP_JOB)
+                {
+                    methodName = nameof(SubscriptionType.JobStopped);
+                    topicName = $"{prefix}{methodName}_{jobNotification.job_order_guid}";
+                }
+                else if (type == JobNotificationType.COMPLETE_JOB)
+                {
+                    methodName = nameof(SubscriptionType.JobCompleted);
+                    topicName = $"{prefix}{methodName}_{jobNotification.job_order_guid}";
+                }
+                else if (type == JobNotificationType.COMPLETE_ITEM)
+                {
+                    methodName = nameof(SubscriptionType.JobStopped);
+                    topicName = $"{prefix}{methodName}_{jobNotification.item_guid}_{jobNotification.job_type}";
+                }
+                
+                await topicEventSender.SendAsync(topicName, jobNotification);
                 return value;
             }
             catch (Exception ex)
@@ -39,7 +80,7 @@ namespace GlobalMQ.GqlTypes
                 GqlUtils.IsAuthorize(config, httpContextAccessor);
                 DateTime endTargetTime = DateTime.Today.AddHours(23).AddMinutes(59).AddSeconds(59);
                 DateTime startTargetTime = DateTime.Today.AddMonths(-1);
-                
+
                 long enddt = ((DateTimeOffset)endTargetTime).ToUnixTimeSeconds();
                 long startdt = ((DateTimeOffset)startTargetTime).ToUnixTimeSeconds();
 
