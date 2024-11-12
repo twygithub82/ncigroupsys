208095247d6640d4b2d96603964a1d98d5a5b018
Wymund <weaiyep2002@gmail.com>
2024-11-12 23:23:17 +0800

    added RepJobOrder

--- backend/GqlMS/Service/IDMS.Repair/LocalModel/RepairRequest.cs
+++ backend/GqlMS/Service/IDMS.Repair/LocalModel/RepairRequest.cs
@@ -20,4 +20,13 @@ namespace IDMS.Repair.GqlTypes.LocalModel
         public string remarks { get; set; }
         public string customer_guid { get; set; }
     }
+
+    public class RepJobOrderRequest
+    {
+        public string guid { get; set; }
+        public string? sot_guid { get; set; }
+        public string? estimate_no { get; set; }
+        public string? remarks { get; set; }
+        public List<job_order?>? job_order {  get; set; }  
+    }
 }
