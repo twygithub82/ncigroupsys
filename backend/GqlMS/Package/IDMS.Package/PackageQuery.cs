895becc0873296355aec24a63155b006b99f948d
Wymund <weaiyep2002@gmail.com>
2024-11-17 16:18:47 +0800

    added package steaming handling

--- backend/GqlMS/Package/IDMS.Package/PackageQuery.cs
+++ backend/GqlMS/Package/IDMS.Package/PackageQuery.cs
@@ -34,7 +34,6 @@ namespace IDMS.Models.Package.GqlTypes
             }
 
             return query;
-
         }
 
         // [Authorize]
@@ -89,7 +88,6 @@ namespace IDMS.Models.Package.GqlTypes
             }
 
             return query;
-
         }
 
 
@@ -159,6 +157,31 @@ namespace IDMS.Models.Package.GqlTypes
 
             return query;
         }
+
+        // [Authorize]
+        [UsePaging(IncludeTotalCount = true, DefaultPageSize = 10)]
+        [UseProjection()]
+        [UseFiltering()]
+        [UseSorting]
+        public IQueryable<package_steaming?> QueryPackageSteaming(ApplicationPackageDBContext context,
+            [Service] IConfiguration config, [Service] IHttpContextAccessor httpContextAccessor)
+        {
+
+            IQueryable<package_steaming> query = null;
+            try
+            {
+
+                GqlUtils.IsAuthorize(config, httpContextAccessor);
+                query = context.package_steaming.Where(i => i.delete_dt == null || i.delete_dt == 0);
+
+            }
+            catch
+            {
+                throw;
+            }
+
+            return query;
+        }
         //public async Task<List<EntityClass_CleaningProcedure>> QueryCleaningProcedures([Service] IConfiguration config, [Service] IHttpContextAccessor httpContextAccessor,EntityClass_CleaningProcedure queryCleaningProcedure,string orderby="guid",int offset = 0, int limit=10)
         //{
         //    List<EntityClass_CleaningProcedure> retval = new List<EntityClass_CleaningProcedure>();
@@ -197,53 +220,5 @@ namespace IDMS.Models.Package.GqlTypes
         //    return retval;
         //}
 
-
-        //public async Task<EntityClass_CleaningProcedureWithStepsAndGroupShort > QueryCleaningProcedureWithStepsAndGroupShort([Service] IConfiguration config, [Service] IHttpContextAccessor httpContextAccessor, string queryCleaningProcedure_guid)
-        //{
-        //    EntityClass_CleaningProcedureWithStepsAndGroupShort retval = new EntityClass_CleaningProcedureWithStepsAndGroupShort();
-        //    try
-        //    {
-        //        var table = "idms.cleaning_procedure";
-        //        GqlUtils.IsAuthorize(config, httpContextAccessor);
-
-        //        string sqlStatement =$"select * from {table} where guid ='{queryCleaningProcedure_guid}'";
-        //        var resultJtoken = await GqlUtils.QueryData(config, sqlStatement);
-        //        var resultList = resultJtoken["result"];
-        //        if (resultList?.Count() > 0)
-        //        {
-        //            var result = resultList[0];
-        //            retval = result.ToObject<EntityClass_CleaningProcedureWithStepsAndGroupShort>();
-        //            //sqlStatement = $"select * from idms.cleaning_steps where guid in (select cleaning_step_guid from idms.cleaning_procedure_steps where cleaning_procedure_guid ='{retval.guid}')";
-        //            sqlStatement = @$"select a.*, b.duration from idms.cleaning_steps a inner join 
-        //                           (select cleaning_step_guid, duration from idms.cleaning_procedure_steps where cleaning_procedure_guid ='{retval.guid}' and delete_dt is null) b on a.guid=b.cleaning_step_guid";
-        //            resultJtoken = await GqlUtils.QueryData(config, sqlStatement);
-        //            resultList = resultJtoken["result"];
-        //            if(resultList?.Count() > 0)
-        //            {
-        //                var clnSteps = resultList.ToObject<List<EntityClass_CleaningStepWithDuration>>();
-        //                retval.CleaningSteps = clnSteps.ToArray();
-        //            }
-        //            if(!string.IsNullOrEmpty(retval.clean_group_guid))
-        //            {
-        //                sqlStatement = $"select * from idms.cleaning_group where guid ='{retval.clean_group_guid}'";
-        //                resultJtoken = await GqlUtils.QueryData(config, sqlStatement);
-        //                resultList = resultJtoken["result"];
-        //                if (resultList?.Count() > 0)
-        //                {
-        //                    var clnGroup = resultList[0];
-        //                    var cleanGroupShort = clnGroup.ToObject<EntityClass_CleaningGroup_Short>();
-        //                    retval.CleaningGroupShort = cleanGroupShort;
-        //                }
-        //            }
-        //        }
-
-        //    }
-        //    catch
-        //    {
-        //        throw;
-        //    }
-
-        //    return retval;
-        //}
     }
 }
