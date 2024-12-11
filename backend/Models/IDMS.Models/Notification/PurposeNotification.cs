5c968ca4b4cb55dcbdb898f7a80f4a8f3c6f824d
Wymund <weaiyep2002@gmail.com>
2024-12-12 06:48:57 +0800

    added purposeChange model class

---
+++ backend/Models/IDMS.Models/Notification/PurposeNotification.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IDMS.Models.Notification
+{
+    public class PurposeNotification
+    {
+        public string purpose {  get; set; }
+        public string tank_status { get; set; }
+        public string sot_guid { get; set; }
+
+    }
+}
