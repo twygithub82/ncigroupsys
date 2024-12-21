2a80e127cc6e5746b825956f91cb62ba7f5e0cc4
Wymund <weaiyep2002@gmail.com>
2024-12-21 08:03:50 +0800

    fixed error by adding foreign key annotation

--- backend/Models/IDMS.Models/Shared/survey_detail.cs
+++ backend/Models/IDMS.Models/Shared/survey_detail.cs
@@ -3,6 +3,7 @@ using IDMS.Models.Master;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,12 +14,16 @@ namespace IDMS.Models.Shared
     {
         [Key]
         public string? guid {  get; set; }
-        public string? customer_company_guid { get; set; }
-        public string? sot_guid {  get; set; }
-        public string? status_cv { get; set; }
-        public string? survey_type { get; set; }
+
+        [ForeignKey("customer_company")]
+        public string customer_company_guid { get; set; }
+
+        [ForeignKey("storing_order_tank")]
+        public string sot_guid {  get; set; }
+        public string status_cv { get; set; }
+        public string survey_type_cv { get; set; }
         public string? remarks { get; set; }
-        public long? survey_dt { get; set; }
+        public long survey_dt { get; set; }
 
         public customer_company? customer_company { get; set; }
         public storing_order_tank? storing_order_tank { get; set; }
