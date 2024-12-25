9ad5985510f576d7c393b85c1d5d761d14b2d659
Wymund <weaiyep2002@gmail.com>
2024-12-25 15:51:52 +0800

    added tariff_cleaning_guid

--- backend/Models/IDMS.Models/Tariff/steaming_exclusive.cs
+++ backend/Models/IDMS.Models/Tariff/steaming_exclusive.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,6 +12,9 @@ namespace IDMS.Models.Tariff
     {
         [Key]
         public string guid {  get; set; }
+
+        [ForeignKey("tariff_cleaning")]
+        public string tariff_cleaning_guid { get; set; }
         public float? temp_min { get; set; }
         public float? temp_max { get; set; }
         public string? remarks { get; set; }
