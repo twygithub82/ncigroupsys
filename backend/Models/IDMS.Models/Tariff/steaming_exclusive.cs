4f09a1d918b7b254b40e2971086753f5a8987a60
Wymund <weaiyep2002@gmail.com>
2024-12-25 15:48:55 +0800

    added steaming_exclusive model

---
+++ backend/Models/IDMS.Models/Tariff/steaming_exclusive.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IDMS.Models.Tariff
+{
+    public class steaming_exclusive: Dates
+    {
+        [Key]
+        public string guid {  get; set; }
+        public float? temp_min { get; set; }
+        public float? temp_max { get; set; }
+        public string? remarks { get; set; }
+        public double? labour { get; set; }
+        public tariff_cleaning? tariff_cleaning {  get; set; } 
+    }
+}
