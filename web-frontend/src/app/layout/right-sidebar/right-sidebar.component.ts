0b67f4074015ede1983b0e97a26cb22475a7193c
danielwongsh <74439785+danielwongsh@users.noreply.github.com>
2024-12-29 23:22:35 +0800

    adjusted some warning from console

--- web-frontend/src/app/layout/right-sidebar/right-sidebar.component.ts
+++ web-frontend/src/app/layout/right-sidebar/right-sidebar.component.ts
@@ -17,23 +17,22 @@ import { NgScrollbar } from 'ngx-scrollbar';
 import { FeatherIconsComponent } from '../../shared/components/feather-icons/feather-icons.component';
 
 @Component({
-    changeDetection: ChangeDetectionStrategy.OnPush,
-    selector: 'app-right-sidebar',
-    templateUrl: './right-sidebar.component.html',
-    styleUrls: ['./right-sidebar.component.scss'],
-    standalone: true,
-    imports: [
-        NgClass,
-        FeatherIconsComponent,
-        NgScrollbar,
-        MatButtonToggleModule,
-        MatSlideToggleModule,
-    ],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  selector: 'app-right-sidebar',
+  templateUrl: './right-sidebar.component.html',
+  styleUrls: ['./right-sidebar.component.scss'],
+  standalone: true,
+  imports: [
+    NgClass,
+    FeatherIconsComponent,
+    NgScrollbar,
+    MatButtonToggleModule,
+    MatSlideToggleModule,
+  ],
 })
 export class RightSidebarComponent
   extends UnsubscribeOnDestroyAdapter
-  implements OnInit, AfterViewInit
-{
+  implements OnInit, AfterViewInit {
   selectedBgColor = 'white';
   maxHeight!: string;
   maxWidth!: string;
@@ -146,7 +145,7 @@ export class RightSidebarComponent
     this.renderer.addClass(this.document.body, 'theme-white');
     const theme = 'light';
     const menuOption = 'menu_light';
-    this.selectedBgColor = 'gold-white';
+    this.selectedBgColor = 'white';
     this.isDarkSidebar = false;
     localStorage.setItem('choose_logoheader', 'logo-white');
     localStorage.setItem('choose_skin', 'theme-white');
