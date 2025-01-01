286c72292872280cfde8a8821c36e641d42684f4
Wymund <weaiyep2002@gmail.com>
2025-01-01 16:40:12 +0800

    add UAT Env handling

--- backend/GqlMS/Tariff/IDMS.Tariff.Application/Program.cs
+++ backend/GqlMS/Tariff/IDMS.Tariff.Application/Program.cs
@@ -8,12 +8,13 @@ using IDMS.Models.Tariff.GqlTypes;
 
 var builder = WebApplication.CreateBuilder(args);
 
+
+string connectionString = builder.Configuration.GetConnectionString("default");
 var JWT_validAudience = builder.Configuration["JWT_VALIDAUDIENCE"];
 var JWT_validIssuer = builder.Configuration["JWT_VALIDISSUER"];
 //var JWT_secretKey = await dbWrapper.GetJWTKey(builder.Configuration["DBService:queryUrl"]);
-var JWT_secretKey = await dbWrapper.GetJWTKey(builder.Configuration.GetConnectionString("DefaultConnection"));
+var JWT_secretKey = await dbWrapper.GetJWTKey(connectionString);
 
-string connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 //builder.Services.AddPooledDbContextFactory<AppDbContext>(o => o.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)).LogTo(Console.WriteLine));
 
 builder.Services.AddPooledDbContextFactory<ApplicationTariffDBContext>(o =>
