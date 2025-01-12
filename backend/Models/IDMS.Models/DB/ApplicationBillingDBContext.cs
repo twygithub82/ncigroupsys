72dd0ca705079c37c3d89f7c28f2cefc9bc1631c
Wymund <weaiyep2002@gmail.com>
2025-01-12 22:47:40 +0800

    added billing and billing_sot model

---
+++ backend/Models/IDMS.Models/DB/ApplicationBillingDBContext.cs
@@ -0,0 +1,22 @@
+﻿using IDMS.Models.Billing;
+using Microsoft.EntityFrameworkCore;
+
+namespace IDMS.Models.DB
+{
+    public class ApplicationBillingDBContext : DbContext
+    {
+        public ApplicationBillingDBContext(DbContextOptions<ApplicationBillingDBContext> options):base(options) 
+        {
+            
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+
+        public DbSet<billing> billing { get; set; }
+        public DbSet<billing_sot> billing_sot { get; set; }
+
+    }
+}
