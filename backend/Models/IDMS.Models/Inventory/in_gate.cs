a209c59b88d802cae15706be9eee8e59cce5a80d
Wymund <weaiyep2002@gmail.com>
2025-01-19 11:06:51 +0800

    added published_by and published_dt for ingate and outgate

--- backend/Models/IDMS.Models/Inventory/in_gate.cs
+++ backend/Models/IDMS.Models/Inventory/in_gate.cs
@@ -23,9 +23,10 @@ namespace IDMS.Models.Inventory
         public string? preinspection_cv { get; set; } = "";
         public long? eir_dt { get; set; }
         public string? remarks { get; set; }
+        public string? publish_by { get; set; }
+        public long? publish_dt { get; set; }
 
         public in_gate_survey? in_gate_survey { get; set; }
-
         public storing_order_tank? tank { get; set; } = null;
     }
 }
