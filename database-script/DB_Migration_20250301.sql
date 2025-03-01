43589db9a283bef5acbce262245f45026082f0b4
danielwongsh <74439785+danielwongsh@users.noreply.github.com>
2025-03-01 16:08:53 +0800

    add db migration script

---
+++ database-script/DB_Migration_20250301.sql
@@ -0,0 +1,10 @@
+ALTER TABLE `idms`.`in_gate_survey` 
+ADD COLUMN `btm_dis_valve_oth` VARCHAR(50) NULL DEFAULT NULL AFTER `right_remarks`,
+ADD COLUMN `btm_dis_valve_spec_oth` VARCHAR(50) NULL DEFAULT NULL AFTER `btm_dis_valve_oth`,
+ADD COLUMN `foot_valve_oth` VARCHAR(50) NULL DEFAULT NULL AFTER `btm_dis_valve_spec_oth`,
+ADD COLUMN `top_dis_valve_oth` VARCHAR(50) NULL DEFAULT NULL AFTER `foot_valve_oth`,
+ADD COLUMN `top_dis_valve_spec_oth` VARCHAR(50) NULL DEFAULT NULL AFTER `top_dis_valve_oth`,
+ADD COLUMN `airline_valve_oth` VARCHAR(50) NULL DEFAULT NULL AFTER `top_dis_valve_spec_oth`,
+ADD COLUMN `airline_valve_conn_oth` VARCHAR(50) NULL DEFAULT NULL AFTER `airline_valve_oth`,
+ADD COLUMN `airline_valve_conn_spec_oth` VARCHAR(50) NULL DEFAULT NULL AFTER `airline_valve_conn_oth`,
+ADD COLUMN `manlid_cover_oth` VARCHAR(50) NULL DEFAULT NULL AFTER `airline_valve_conn_spec_oth`;
