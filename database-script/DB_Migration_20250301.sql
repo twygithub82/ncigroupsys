63df2e4bd0fdb18be2fddf9b270f9bc34fa5bc69
weishunDEV <167762800+weishunDEV@users.noreply.github.com>
2025-03-03 02:49:29 +0800

    Update DB_Migration_20250301.sql

--- database-script/DB_Migration_20250301.sql
+++ database-script/DB_Migration_20250301.sql
@@ -11,3 +11,41 @@ ADD COLUMN `manlid_cover_oth` VARCHAR(50) NULL DEFAULT NULL AFTER `airline_valve
 
 ALTER TABLE `idms`.`out_gate` 
 ADD COLUMN `yard_cv` VARCHAR(20) NULL AFTER `running_number`;
+
+ALTER TABLE `idms`.`customer_company` 
+ADD COLUMN `country_code` VARCHAR(5) NULL DEFAULT NULL AFTER `def_tank_guid`;
+
+CREATE TABLE `cleaning_formula` (
+  `guid` varchar(36) NOT NULL,
+  `description` varchar(255) DEFAULT NULL,
+  `duration` double DEFAULT NULL,
+  `update_dt` bigint DEFAULT NULL,
+  `update_by` varchar(45) DEFAULT NULL,
+  `create_dt` bigint DEFAULT NULL,
+  `create_by` varchar(45) DEFAULT NULL,
+  `delete_dt` bigint DEFAULT NULL,
+  PRIMARY KEY (`guid`),
+  UNIQUE KEY `guid_UNIQUE` (`guid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+CREATE TABLE `cleaning_method_formula` (
+  `guid` varchar(36) NOT NULL,
+  `method_guid` varchar(36) DEFAULT NULL,
+  `formula_guid` varchar(36) DEFAULT NULL,
+  `sequence` int DEFAULT NULL,
+  `update_dt` bigint DEFAULT NULL,
+  `update_by` varchar(45) DEFAULT NULL,
+  `create_dt` bigint DEFAULT NULL,
+  `create_by` varchar(45) DEFAULT NULL,
+  `delete_dt` bigint DEFAULT NULL,
+  PRIMARY KEY (`guid`),
+  UNIQUE KEY `guid_UNIQUE` (`guid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+ALTER TABLE `idms`.`tank` 
+ADD COLUMN `preinspect` TINYINT NULL DEFAULT 1 AFTER `description`,
+ADD COLUMN `lift_on` TINYINT NULL DEFAULT 1 AFTER `preinspect`,
+ADD COLUMN `lift_off` TINYINT NULL DEFAULT 1 AFTER `lift_on`,
+ADD COLUMN `gate_in` TINYINT NULL DEFAULT 1 AFTER `lift_off`,
+ADD COLUMN `gate_out` TINYINT NULL DEFAULT 1 AFTER `gate_in`,
+ADD COLUMN `iso_format` TINYINT NULL DEFAULT 1 AFTER `gate_out`;
