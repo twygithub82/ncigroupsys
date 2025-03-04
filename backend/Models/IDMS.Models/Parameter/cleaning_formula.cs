31d73a53dbc99060ef4168006cee230e4eb3131a
Wymund <weaiyep2002@gmail.com>
2025-03-04 11:15:54 +0800

    added cleaning_formula and cleaning_method_formula schema

---
+++ backend/Models/IDMS.Models/Parameter/cleaning_formula.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace IDMS.Models.Parameter
+{
+    public class cleaning_formula : Dates
+    {
+        [Key]
+        public string? guid { get; set; }
+
+        public string? description { get; set; }
+
+        public double? duration { get; set; }
+        [UseFiltering]
+        public IEnumerable<cleaning_method_formula?>? cleaning_method_formula { get; set; }
+    }
+}
---
+++ backend/Models/IDMS.Models/Parameter/cleaning_method_formula.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace IDMS.Models.Parameter
+{
+    public class cleaning_method_formula : Dates
+    {
+        [Key]
+        public string? guid { get; set; }
+
+        [ForeignKey("cleaning_method")]
+        public string? method_guid { get; set; }
+
+        [ForeignKey("cleaning_formula")]
+        public string? formula_guid { get; set; }
+        public int? sequence { get; set; }
+        [UseFiltering]
+        public cleaning_method? cleaning_method { get; set; }
+        [UseFiltering]
+        public cleaning_formula? cleaning_formula { get; set; }
+    }
+}
