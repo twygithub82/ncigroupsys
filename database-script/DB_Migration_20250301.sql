6e398801e8a5ce466e57760d2a314deaae59da13
Wymund <weaiyep2002@gmail.com>
2025-03-04 10:46:13 +0800

    update latest info into script

--- database-script/DB_Migration_20250301.sql
+++ database-script/DB_Migration_20250301.sql
@@ -9,6 +9,19 @@ ADD COLUMN `airline_valve_conn_oth` VARCHAR(50) NULL DEFAULT NULL AFTER `airline
 ADD COLUMN `airline_valve_conn_spec_oth` VARCHAR(50) NULL DEFAULT NULL AFTER `airline_valve_conn_oth`,
 ADD COLUMN `manlid_cover_oth` VARCHAR(50) NULL DEFAULT NULL AFTER `airline_valve_conn_spec_oth`;
 
+
+ALTER TABLE `idms`.`out_gate_survey` 
+ADD COLUMN `btm_dis_valve_oth` VARCHAR(50) NULL DEFAULT NULL AFTER `right_remarks`,
+ADD COLUMN `btm_dis_valve_spec_oth` VARCHAR(50) NULL DEFAULT NULL AFTER `btm_dis_valve_oth`,
+ADD COLUMN `foot_valve_oth` VARCHAR(50) NULL DEFAULT NULL AFTER `btm_dis_valve_spec_oth`,
+ADD COLUMN `top_dis_valve_oth` VARCHAR(50) NULL DEFAULT NULL AFTER `foot_valve_oth`,
+ADD COLUMN `top_dis_valve_spec_oth` VARCHAR(50) NULL DEFAULT NULL AFTER `top_dis_valve_oth`,
+ADD COLUMN `airline_valve_oth` VARCHAR(50) NULL DEFAULT NULL AFTER `top_dis_valve_spec_oth`,
+ADD COLUMN `airline_valve_conn_oth` VARCHAR(50) NULL DEFAULT NULL AFTER `airline_valve_oth`,
+ADD COLUMN `airline_valve_conn_spec_oth` VARCHAR(50) NULL DEFAULT NULL AFTER `airline_valve_conn_oth`,
+ADD COLUMN `manlid_cover_oth` VARCHAR(50) NULL DEFAULT NULL AFTER `airline_valve_conn_spec_oth`;
+
+
 ALTER TABLE `idms`.`out_gate` 
 ADD COLUMN `yard_cv` VARCHAR(20) NULL AFTER `running_number`;
 
