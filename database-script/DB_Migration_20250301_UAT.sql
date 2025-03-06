b6d17b6c8a2a00b9fdf02c7b4bd23b97f0c013a9
danielwongsh <74439785+danielwongsh@users.noreply.github.com>
2025-03-06 19:24:28 +0800

    migrate DB_Migration_20250301 to UAT

--- database-script/DB_Migration_20250301_UAT.sql
+++ database-script/DB_Migration_20250301_UAT.sql
@@ -9,7 +9,6 @@ ADD COLUMN `airline_valve_conn_oth` VARCHAR(50) NULL DEFAULT NULL AFTER `airline
 ADD COLUMN `airline_valve_conn_spec_oth` VARCHAR(50) NULL DEFAULT NULL AFTER `airline_valve_conn_oth`,
 ADD COLUMN `manlid_cover_oth` VARCHAR(50) NULL DEFAULT NULL AFTER `airline_valve_conn_spec_oth`;
 
-
 ALTER TABLE `idms`.`out_gate_survey` 
 ADD COLUMN `btm_dis_valve_oth` VARCHAR(50) NULL DEFAULT NULL AFTER `right_remarks`,
 ADD COLUMN `btm_dis_valve_spec_oth` VARCHAR(50) NULL DEFAULT NULL AFTER `btm_dis_valve_oth`,
@@ -20,7 +19,6 @@ ADD COLUMN `airline_valve_oth` VARCHAR(50) NULL DEFAULT NULL AFTER `top_dis_valv
 ADD COLUMN `airline_valve_conn_oth` VARCHAR(50) NULL DEFAULT NULL AFTER `airline_valve_oth`,
 ADD COLUMN `airline_valve_conn_spec_oth` VARCHAR(50) NULL DEFAULT NULL AFTER `airline_valve_conn_oth`,
 ADD COLUMN `manlid_cover_oth` VARCHAR(50) NULL DEFAULT NULL AFTER `airline_valve_conn_spec_oth`;
-
 
 ALTER TABLE `idms`.`out_gate` 
 ADD COLUMN `yard_cv` VARCHAR(20) NULL AFTER `running_number`;
