f1562c5e6a632127079a625bc9273adfcf181f8f
danielwongsh <74439785+danielwongsh@users.noreply.github.com>
2025-03-26 21:51:48 +0800

    change job_no_remarks and last_cargo_remarks position

--- database-script/DB_Migration_20250324.sql
+++ database-script/DB_Migration_20250324.sql
@@ -5,12 +5,6 @@ INSERT INTO `idms`.`code_values` (`guid`, `description`, `code_val_type`, `code_
 INSERT INTO `idms`.`code_values` (`guid`, `description`, `code_val_type`, `code_val`) VALUES ('615', 'Repair Cost', 'SALES_COST_TYPE', 'REPAIR');
 INSERT INTO `idms`.`code_values` (`guid`, `description`, `code_val_type`, `code_val`) VALUES ('616', 'Steam Cost', 'SALES_COST_TYPE', 'STEAMING');
 
-
-
-ALTER TABLE `idms`.`steaming` 
-ADD COLUMN `total_material_cost` DOUBLE NULL DEFAULT NULL AFTER `owner_billing_guid`,
-ADD COLUMN `total_labour_cost` DOUBLE NULL DEFAULT NULL AFTER `total_material_cost`;
-
 ALTER TABLE `idms`.`billing_sot` 
 ADD COLUMN `gate_in` TINYINT NULL DEFAULT NULL AFTER `lift_off`,
 ADD COLUMN `gate_out` TINYINT NULL DEFAULT NULL AFTER `gate_in`;
@@ -18,6 +12,9 @@ ADD COLUMN `gate_out` TINYINT NULL DEFAULT NULL AFTER `gate_in`;
 ALTER TABLE `idms`.`storing_order_tank` 
 ADD COLUMN `job_no_remarks` VARCHAR(250) NULL DEFAULT NULL COMMENT 'For overwrite job no' AFTER `delete_dt`,
 ADD COLUMN `last_cargo_remarks` VARCHAR(250) NULL DEFAULT NULL COMMENT 'For overwrite last cargo' AFTER `job_no_remarks`;
+ALTER TABLE `idms`.`storing_order_tank` 
+CHANGE COLUMN `job_no_remarks` `job_no_remarks` VARCHAR(250) NULL DEFAULT NULL COMMENT 'For overwrite job no' AFTER `last_release_dt`,
+CHANGE COLUMN `last_cargo_remarks` `last_cargo_remarks` VARCHAR(250) NULL DEFAULT NULL COMMENT 'For overwrite last cargo' AFTER `job_no_remarks`;
 
 ALTER TABLE `idms`.`billing_sot` 
 ADD COLUMN `depot_cost_remarks` VARCHAR(250) NULL DEFAULT NULL COMMENT 'For overwrite depot cost' AFTER `delete_dt`;
