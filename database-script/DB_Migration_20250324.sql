fb78bbbd8d07984d6227f10b05299e0bfeecbaa0
Wymund <weaiyep2002@gmail.com>
2025-03-26 21:42:07 +0800

    added total_material_cost and total_labour_cost in steaming

--- database-script/DB_Migration_20250324.sql
+++ database-script/DB_Migration_20250324.sql
@@ -4,3 +4,9 @@ INSERT INTO `idms`.`code_values` (`guid`, `description`, `code_val_type`, `code_
 INSERT INTO `idms`.`code_values` (`guid`, `description`, `code_val_type`, `code_val`) VALUES ('614', 'Cleaning Cost', 'SALES_COST_TYPE', 'CLEANING');
 INSERT INTO `idms`.`code_values` (`guid`, `description`, `code_val_type`, `code_val`) VALUES ('615', 'Repair Cost', 'SALES_COST_TYPE', 'REPAIR');
 INSERT INTO `idms`.`code_values` (`guid`, `description`, `code_val_type`, `code_val`) VALUES ('616', 'Steam Cost', 'SALES_COST_TYPE', 'STEAMING');
+
+
+
+ALTER TABLE `idms`.`steaming` 
+ADD COLUMN `total_material_cost` DOUBLE NULL DEFAULT NULL AFTER `owner_billing_guid`,
+ADD COLUMN `total_labour_cost` DOUBLE NULL DEFAULT NULL AFTER `total_material_cost`;
