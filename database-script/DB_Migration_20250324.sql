5e828fb5e175797fc2e5540a6f87821867582e87
danielwongsh <74439785+danielwongsh@users.noreply.github.com>
2025-03-26 20:49:36 +0800

    add column in billing_sot
    gate_in and gate_out

--- database-script/DB_Migration_20250324.sql
+++ database-script/DB_Migration_20250324.sql
@@ -4,3 +4,7 @@ INSERT INTO `idms`.`code_values` (`guid`, `description`, `code_val_type`, `code_
 INSERT INTO `idms`.`code_values` (`guid`, `description`, `code_val_type`, `code_val`) VALUES ('614', 'Cleaning Cost', 'SALES_COST_TYPE', 'CLEANING');
 INSERT INTO `idms`.`code_values` (`guid`, `description`, `code_val_type`, `code_val`) VALUES ('615', 'Repair Cost', 'SALES_COST_TYPE', 'REPAIR');
 INSERT INTO `idms`.`code_values` (`guid`, `description`, `code_val_type`, `code_val`) VALUES ('616', 'Steam Cost', 'SALES_COST_TYPE', 'STEAMING');
+
+ALTER TABLE `idms`.`billing_sot` 
+ADD COLUMN `gate_in` TINYINT NULL DEFAULT NULL AFTER `lift_off`,
+ADD COLUMN `gate_out` TINYINT NULL DEFAULT NULL AFTER `gate_in`;
