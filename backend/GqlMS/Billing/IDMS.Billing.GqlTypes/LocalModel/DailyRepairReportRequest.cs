f7f176d98a72a87ba45dd4881e7992210ef1ae8b
Wymund <weaiyep2002@gmail.com>
2025-04-01 21:08:59 +0800

    refactor code only

--- backend/GqlMS/Billing/IDMS.Billing.GqlTypes/LocalModel/DailyRepairReportRequest.cs
+++ backend/GqlMS/Billing/IDMS.Billing.GqlTypes/LocalModel/DailyRepairReportRequest.cs
@@ -21,31 +21,20 @@ namespace IDMS.Billing.GqlTypes.LocalModel
         public List<string?>? team { get; set; }
     }
 
+    [NotMapped]
     public class DailyRepair
     {
-        [NotMapped]
         public List<string?>? repair_type { get; set; }
-        [NotMapped]
         public string? customer_code { get; set; }
-        [NotMapped]
         public string? tank_no { get; set; }
-        [NotMapped]
         public string? eir_no { get; set; }
-        [NotMapped]
         public long? estimate_start_date { get; set; }
-        [NotMapped]
         public long? estimate_end_date { get; set; }
-        [NotMapped]
         public long? allocation_start_date { get; set; }
-        [NotMapped]
         public long? allocation_end_date { get; set; }
-        [NotMapped]
-        public long approved_start_date { get; set; }
-        [NotMapped]
-        public long approved_end_date { get; set; }
-        [NotMapped]
+        public long? approved_start_date { get; set; }
+        public long? approved_end_date { get; set; }
         public long? qc_start_date { get; set; }
-        [NotMapped]
         public long? qc_end_date { get; set; }
     }
 }
