deeb83405b57fc9928707ee52cd9419c323aacdd
Wymund <weaiyep2002@gmail.com>
2025-04-19 01:16:37 +0800

    add invoice type code value

---
+++ database-script/DB_Migration_20250418.sql
@@ -0,0 +1,19 @@
+---- Added invoice_type in code_value table ----------------
+insert into code_values (guid, description, code_val_type, code_val, child_code, sequence, delete_dt, create_dt, create_by, update_dt, update_by) values 
+('70', 'Cleaning', 'INVOICE_TYPE', 'CLEANING', null, null, null, null, null,null,null);
+insert into code_values (guid, description, code_val_type, code_val, child_code, sequence, delete_dt, create_dt, create_by, update_dt, update_by) values 
+('71', 'Repair', 'INVOICE_TYPE', 'REPAIR', null, null, null, null, null,null,null);
+insert into code_values (guid, description, code_val_type, code_val, child_code, sequence, delete_dt, create_dt, create_by, update_dt, update_by) values 
+('72', 'Steaming', 'INVOICE_TYPE', 'STEAMING', null, null, null, null, null,null,null);
+insert into code_values (guid, description, code_val_type, code_val, child_code, sequence, delete_dt, create_dt, create_by, update_dt, update_by) values 
+('73', 'PreInsepction', 'INVOICE_TYPE', 'PREINSPECTION', null, null, null, null, null,null,null);
+insert into code_values (guid, description, code_val_type, code_val, child_code, sequence, delete_dt, create_dt, create_by, update_dt, update_by) values 
+('74', 'Lift-On', 'INVOICE_TYPE', 'LIFT_ON', null, null, null, null, null,null,null);
+insert into code_values (guid, description, code_val_type, code_val, child_code, sequence, delete_dt, create_dt, create_by, update_dt, update_by) values 
+('75', 'Lift-Off', 'INVOICE_TYPE', 'LIFT_OFF', null, null, null, null, null,null,null);
+insert into code_values (guid, description, code_val_type, code_val, child_code, sequence, delete_dt, create_dt, create_by, update_dt, update_by) values 
+('76', 'Gate-In', 'INVOICE_TYPE', 'GATE_IN', null, null, null, null, null,null,null);
+insert into code_values (guid, description, code_val_type, code_val, child_code, sequence, delete_dt, create_dt, create_by, update_dt, update_by) values 
+('77', 'Gate-Out', 'INVOICE_TYPE', 'GATE_OUT', null, null, null, null, null,null,null);
+insert into code_values (guid, description, code_val_type, code_val, child_code, sequence, delete_dt, create_dt, create_by, update_dt, update_by) values 
+('78', 'Storage', 'INVOICE_TYPE', 'STORAGE', null, null, null, null, null,null,null);
