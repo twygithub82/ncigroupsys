fbc245f7bd7c831330d9c0e1421275e7423366b5
Wymund <weaiyep2002@gmail.com>
2025-04-18 17:49:44 +0800

    modify add/update surveyDetail logic

--- backend/GqlMS/Inventory/IDMS.Inventory/LocalModel/PeriodicTestRequest.cs
+++ backend/GqlMS/Inventory/IDMS.Inventory/LocalModel/PeriodicTestRequest.cs
@@ -1,16 +1,20 @@
 ﻿using IDMS.Models.Shared;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace IDMS.Inventory.GqlTypes.LocalModel
 {
+    [NotMapped]
     public class PeriodicTestRequest
     {
         public string tank_no { get; set; }
         public string last_test_cv { get; set; }
         public string next_test_cv { get; set; }
+        public long test_dt { get; set; }   
+
     }
 }
