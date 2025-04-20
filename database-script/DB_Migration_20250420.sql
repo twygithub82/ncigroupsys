6a3c4a1650210e92819f49ba08f30399ff70eb70
Wymund <weaiyep2002@gmail.com>
2025-04-20 22:08:47 +0800

    added category guid DB script

--- database-script/DB_Migration_20250420.sql
+++ database-script/DB_Migration_20250420.sql
@@ -1,2 +1,6 @@
 ALTER TABLE `idms`.`job_order` 
 ADD INDEX `job_type_cv` (`job_type_cv` ASC) VISIBLE;
+
+
+ALTER TABLE `idms`.`cleaning_method` 
+ADD COLUMN `category_guid` VARCHAR(36) NULL DEFAULT NULL AFTER `guid`;
