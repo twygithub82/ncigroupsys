222fa168e0e36e6b82db13bfcd78231e62af5726
Wymund <weaiyep2002@gmail.com>
2025-04-23 20:52:27 +0800

    modify logic to cater billing storage cost

--- backend/GqlMS/Billing/IDMS.Billing.GqlTypes/LocalModel/BillingEstimateRequest.cs
+++ backend/GqlMS/Billing/IDMS.Billing.GqlTypes/LocalModel/BillingEstimateRequest.cs
@@ -1,20 +1,32 @@
 ﻿using HotChocolate;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.CompilerServices;
-using System.Text;
-using System.Threading.Tasks;
+using IDMS.Models.Shared;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Drawing;
 
 namespace IDMS.Billing.GqlTypes.LocalModel
 {
+    [NotMapped]
     public class BillingEstimateRequest
     {
-        public string process_guid {  get; set; }
+        public string process_guid { get; set; }
         public string process_type { get; set; }
         public string billing_party { get; set; } //OWNER, CUSTOMER
         public string action { get; set; }
         [GraphQLIgnore]
         public string? billing_guid { get; set; }
+    }
+
+    [NotMapped]
+    public class StorageDetailRequest
+    {
+        public string action { get; set; }
+        public string? guid { get; set; }
+        public string sot_guid { get; set; }
+        public long start_dt { get; set; }
+        public long end_dt { get; set; }
+        public double total_cost { get; set; }
+        public int remaining_free_storage { get; set; }
+        public string state_cv { get; set; }
+        public string? remarks { get; set; }
     }
 }
