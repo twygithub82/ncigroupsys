564d6777aa47bbbd64214b9385d017a44e6b3b9e
danielwongsh <74439785+danielwongsh@users.noreply.github.com>
2025-05-07 21:19:01 +0800

    update add alter steaming table to db migration

--- database-script/DB_Migration_20250507.sql
+++ database-script/DB_Migration_20250507.sql
@@ -5,3 +5,9 @@ UPDATE `idms`.`code_values` SET `description` = 'Cancelled' WHERE (`guid` = '30'
 UPDATE `idms`.`code_values` SET `description` = 'Cancelled' WHERE (`guid` = '508');
 UPDATE `idms`.`code_values` SET `description` = 'Cancelled' WHERE (`guid` = '545');
 UPDATE `idms`.`code_values` SET `description` = 'Cancelled' WHERE (`guid` = '571');
+
+ALTER TABLE idms.steaming 
+ADD COLUMN est_hour DOUBLE NULL AFTER est_cost,
+ADD COLUMN total_hour DOUBLE NULL AFTER est_hour,
+ADD COLUMN flat_rate TINYINT NULL AFTER total_hour,
+ADD COLUMN hourly_rate DOUBLE NULL AFTER flat_rate;
