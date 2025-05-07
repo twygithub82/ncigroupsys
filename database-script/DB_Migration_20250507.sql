34d9b61809bf0305e7b22e6af5a72431ff22c532
danielwongsh <74439785+danielwongsh@users.noreply.github.com>
2025-05-07 21:16:25 +0800

    add db migration script 0507

---
+++ database-script/DB_Migration_20250507.sql
@@ -0,0 +1,7 @@
+UPDATE `idms`.`code_values` SET `description` = 'Cancelled' WHERE (`guid` = '8');
+UPDATE `idms`.`code_values` SET `description` = 'Cancelled' WHERE (`guid` = '149');
+UPDATE `idms`.`code_values` SET `description` = 'Cancelled' WHERE (`guid` = '228');
+UPDATE `idms`.`code_values` SET `description` = 'Cancelled' WHERE (`guid` = '30');
+UPDATE `idms`.`code_values` SET `description` = 'Cancelled' WHERE (`guid` = '508');
+UPDATE `idms`.`code_values` SET `description` = 'Cancelled' WHERE (`guid` = '545');
+UPDATE `idms`.`code_values` SET `description` = 'Cancelled' WHERE (`guid` = '571');
