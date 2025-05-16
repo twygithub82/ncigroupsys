a7080853dc03d1dda10496aadcb428becc250612
boonhong <boonhong@toppannext.com>
2025-05-16 22:00:03 +0800

    lolo dialog update

--- web-frontend/src/app/admin/billing/depot-cost/LOLO/form-dialog/form-dialog.component.ts
+++ web-frontend/src/app/admin/billing/depot-cost/LOLO/form-dialog/form-dialog.component.ts
@@ -44,7 +44,7 @@ export interface DialogData {
 }
 
 @Component({
-  selector: 'app-gate-billing-form-dialog',
+  selector: 'app-lolo-billing-form-dialog',
   templateUrl: './form-dialog.component.html',
   styleUrls: ['./form-dialog.component.scss'],
   providers: [provideNgxMask()],
