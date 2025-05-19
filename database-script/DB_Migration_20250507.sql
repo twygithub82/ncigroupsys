9ad7f6c310f9710697cce6fa3fd4b1ed15d7b081
danielwongsh <74439785+danielwongsh@users.noreply.github.com>
2025-05-19 22:49:33 +0800

    add user_role table

--- database-script/DB_Migration_20250507.sql
+++ database-script/DB_Migration_20250507.sql
@@ -75,3 +75,15 @@ CREATE TABLE `user_functions` (
   PRIMARY KEY (`guid`),
   UNIQUE KEY `guid_UNIQUE` (`guid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+CREATE TABLE user_role (
+  guid varchar(36) NOT NULL,
+  user_guid varchar(36) DEFAULT NULL,
+  role_guid varchar(36) DEFAULT NULL,
+  create_by varchar(45) DEFAULT NULL,
+  create_dt bigint DEFAULT NULL,
+  update_by varchar(45) DEFAULT NULL,
+  update_dt bigint DEFAULT NULL,
+  delete_dt bigint DEFAULT NULL,
+  PRIMARY KEY (guid)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
