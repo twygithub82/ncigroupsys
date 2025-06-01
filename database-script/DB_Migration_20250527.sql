073ebb4b33625ac73ea7cf76eadf86245cec410e
danielwongsh <74439785+danielwongsh@users.noreply.github.com>
2025-06-01 19:07:24 +0800

    update booking tank status sequence

--- database-script/DB_Migration_20250527.sql
+++ database-script/DB_Migration_20250527.sql
@@ -6,3 +6,7 @@ INSERT INTO idms.code_values (guid, description, code_val_type, code_val, sequen
 INSERT INTO idms.code_values (guid, description, code_val_type, code_val, sequence) VALUES ('624', 'Drum', 'RESIDUE_UNIT', 'DRUM', '3');
 INSERT INTO idms.code_values (guid, description, code_val_type, code_val, sequence) VALUES ('625', 'IBC', 'RESIDUE_UNIT', 'IBC', '4');
 INSERT INTO idms.code_values (guid, description, code_val_type, code_val, sequence) VALUES ('626', 'Job', 'RESIDUE_UNIT', 'JOB', '5');
+
+UPDATE `idms`.`code_values` SET `sequence` = '4' WHERE (`guid` = '31');
+UPDATE `idms`.`code_values` SET `sequence` = '5' WHERE (`guid` = '33');
+UPDATE `idms`.`code_values` SET `sequence` = '6' WHERE (`guid` = '38');
