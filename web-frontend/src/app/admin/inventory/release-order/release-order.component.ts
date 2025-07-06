c962819cfee1e1d04fdd13afde4ec2ee48c25b53
Wymund <weaiyep2002@gmail.com>
2025-07-06 23:04:50 +0800

    fixed 2th July UI issue

--- web-frontend/src/app/admin/inventory/release-order/release-order.component.ts
+++ web-frontend/src/app/admin/inventory/release-order/release-order.component.ts
@@ -225,7 +225,7 @@ export class ReleaseOrderComponent extends UnsubscribeOnDestroyAdapter implement
       etr_dt_end: [''],
       purpose: [''],
       release_dt: [''],
-      due_dt:['']
+      due_dt: ['']
     });
   }
 
@@ -240,18 +240,18 @@ export class ReleaseOrderComponent extends UnsubscribeOnDestroyAdapter implement
         'select',
         'ro_no',
         'customer_code',
-        'ro_dt',
-        'status',
         'no_of_tanks',
+        'status',
+        'ro_dt',
         'actions'
       ];
     } else {
       this.displayedColumns = [
         'ro_no',
         'customer_code',
-        'ro_dt',
-        'status',
         'no_of_tanks',
+        'status',
+        'ro_dt',
         'actions'
       ];
     }
@@ -551,13 +551,13 @@ export class ReleaseOrderComponent extends UnsubscribeOnDestroyAdapter implement
     const where: any = {};
 
     if (this.searchForm!.get('due_dt')?.value) {
-       var dueDt = this.searchForm!.get('due_dt')?.value;
-       where.and = [
+      var dueDt = this.searchForm!.get('due_dt')?.value;
+      where.and = [
         // { or:[{ delete_dt:{eq: null}},{ delete_dt:{eq:0}}]},
-        {release_dt: {lte:dueDt  } },
-        {status_cv:{in:['PENDING']}}]  
-    
-  }
+        { release_dt: { lte: dueDt } },
+        { status_cv: { in: ['PENDING'] } }]
+
+    }
 
     if (this.searchForm!.get('ro_no')?.value) {
       where.ro_no = { contains: this.searchForm!.get('ro_no')?.value?.trim() };
