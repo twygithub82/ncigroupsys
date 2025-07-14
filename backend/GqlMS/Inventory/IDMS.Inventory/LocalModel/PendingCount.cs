84c3d9ee2b38f27eafed1d0cb5838593ccaaef39
Wymund <weaiyep2002@gmail.com>
2025-07-14 23:23:41 +0800

    added process pending count

---
+++ backend/GqlMS/Inventory/IDMS.Inventory/LocalModel/PendingCount.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IDMS.Inventory.GqlTypes.LocalModel
+{
+    [NotMapped]
+    public class PendingProcessCount
+    {
+        public int Pending_Cleaning_Count { get; set; } = 0;
+        public int Pending_Residue_Count { get; set; } = 0;
+        public int Pending_Steaming_Count { get; set; } = 0;
+        public int Pending_Estimate_Count { get; set; } = 0;
+    }
+}
