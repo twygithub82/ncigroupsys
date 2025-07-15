5a8223e88044a4d5c2047ff4eb9c5fcb639212af
Wymund <weaiyep2002@gmail.com>
2025-07-16 00:39:01 +0800

    added queryTeamWithCount

---
+++ backend/GqlMS/Service/IDMS.Service/LocalModel/TeamResult.cs
@@ -0,0 +1,18 @@
+﻿using IDMS.Models.Shared;
+using IDMS.Models.Tariff;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IDMS.Service.GqlTypes.LocalModel
+{
+    [NotMapped]
+    public class TeamResult
+    {
+        public team team { get; set; }
+        public int assign_count { get; set; }
+    }
+}
