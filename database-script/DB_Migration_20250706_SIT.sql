e1478333875ce67b28d94d978a67ef2587079239
danielwongsh <74439785+danielwongsh@users.noreply.github.com>
2025-07-15 23:16:29 +0800

    add role and functions for kiosk in out gate

--- database-script/DB_Migration_20250706_SIT.sql
+++ database-script/DB_Migration_20250706_SIT.sql
@@ -144,3 +144,16 @@ DELETE FROM role WHERE guid = @new_guid;
 
 -- Check the audit logs
 SELECT * FROM audit_logs;
+
+
+-- Kiosk Role
+INSERT INTO `idms`.`role` (`guid`, `department`, `position`, `code`, `description`) VALUES ('2a8999bc25cc4c2fa7d3dd6871ab638c', 'KIOSK', 'IN_GATE', 'KIOSK_IN_GATE', 'Kiosk In Gate Role');
+INSERT INTO `idms`.`role` (`guid`, `department`, `position`, `code`, `description`) VALUES ('66509c2d3af64a0ebdbe3ac74336aa0a', 'KIOSK', 'OUT_GATE', 'KIOSK_OUT_GATE', 'Kiosk Out Gate Role');
+
+-- Kiosk In Gate and Out Gate
+INSERT INTO `idms`.`functions` (`guid`, `module`, `submodule`, `action`, `code`) VALUES ('f6bfbb4982334f9193314ba288bcddad', 'KIOSK', 'IN_GATE', 'ADD', 'KIOSK_IN_GATE_ADD');
+INSERT INTO `idms`.`functions` (`guid`, `module`, `submodule`, `action`, `code`) VALUES ('2cba511ed21a47738bd0c613474cc133', 'KIOSK', 'OUT_GATE', 'ADD', 'KIOSK_OUT_GATE_ADD');
+
+-- Kiosk role functions
+INSERT INTO `idms`.`role_functions` (`guid`, `functions_guid`, `role_guid`) VALUES ('399ee14ed75e41cb93e9ef0506a8f03b', 'f6bfbb4982334f9193314ba288bcddad', '2a8999bc25cc4c2fa7d3dd6871ab638c');
+INSERT INTO `idms`.`role_functions` (`guid`, `functions_guid`, `role_guid`) VALUES ('980c3153065548749cb7a6a09b883461', '2cba511ed21a47738bd0c613474cc133', '66509c2d3af64a0ebdbe3ac74336aa0a');
