0c57a4fbf461edd6ee20f4669ba95272e3784f93
Wymund <weaiyep2002@gmail.com>
2025-07-15 22:21:07 +0800

    added new field to user

--- backend/Authentication/IDMS.UserAuthentication/Models/ApplicationUser.cs
+++ backend/Authentication/IDMS.UserAuthentication/Models/ApplicationUser.cs
@@ -4,13 +4,11 @@ namespace IDMS.UserAuthentication.Models
 {
     public class ApplicationUser:IdentityUser
     {
-
         public int CorporateID { get; set; }
-
         public bool isStaff { get; set; } = false;
-
-
         public Guid? CurrentSessionId { get; set; } = null;
+        public string? ActivationCode { get; set; } = string.Empty;
+        public string? LicenseToken { get; set; } = string.Empty;
 
     }
 }
