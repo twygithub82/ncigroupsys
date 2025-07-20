afaa38290b10a66dd88ece3e02759ea3216733b9
danielwongsh <74439785+danielwongsh@users.noreply.github.com>
2025-07-20 21:07:12 +0800

    merge 20250716 to 20250706

--- database-script/DB_Migration_20250706_SIT.sql
+++ database-script/DB_Migration_20250706_SIT.sql
@@ -20,3 +20,7 @@ INSERT INTO `idms`.`functions` (`guid`, `module`, `submodule`, `action`, `code`)
 -- Kiosk role functions
 INSERT INTO `idms`.`role_functions` (`guid`, `functions_guid`, `role_guid`) VALUES ('399ee14ed75e41cb93e9ef0506a8f03b', 'f6bfbb4982334f9193314ba288bcddad', '2a8999bc25cc4c2fa7d3dd6871ab638c');
 INSERT INTO `idms`.`role_functions` (`guid`, `functions_guid`, `role_guid`) VALUES ('980c3153065548749cb7a6a09b883461', '2cba511ed21a47738bd0c613474cc133', '66509c2d3af64a0ebdbe3ac74336aa0a');
+
+INSERT INTO `idms`.`code_values` (`guid`, `description`, `code_val_type`, `code_val`) VALUES ('5290', 'Residue Disposal', 'DEPARTMENT', 'RESIDUE_DISPOSAL');
+INSERT INTO `idms`.`code_values` (`guid`, `description`, `code_val_type`, `code_val`) VALUES ('5291', 'Dashboard', 'DEPARTMENT', 'DASHBOARD');
+INSERT INTO `idms`.`code_values` (`guid`, `description`, `code_val_type`, `code_val`) VALUES ('5292', 'Steaming', 'DEPARTMENT', 'STEAM');
