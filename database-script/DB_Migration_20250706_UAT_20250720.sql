b7c1e64ea83cfcef42a4d7cf2959b1cd8b9f43f9
Wymund <weaiyep2002@gmail.com>
2025-07-20 22:28:32 +0800

    migrate DB script to UAT

--- database-script/DB_Migration_20250706_UAT_20250720.sql
+++ database-script/DB_Migration_20250706_UAT_20250720.sql
@@ -1,13 +1,15 @@
-CREATE TABLE IF NOT EXISTS audit_logs (
-    audit_id INT AUTO_INCREMENT PRIMARY KEY,
-    table_name VARCHAR(255) NOT NULL,
-    operation ENUM('INSERT', 'UPDATE', 'DELETE') NOT NULL,
-    record_id VARCHAR(36), -- Stores the ID (GUID) of the record that was affected
-    old_data LONGTEXT, -- Stores the old row data as a string
-    new_data LONGTEXT, -- Stores the new row data as a string
-    changed_by VARCHAR(255), -- MySQL function to get current user
-    change_timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
+CREATE TABLE `audit_logs` (
+  `guid` varchar(36) NOT NULL,
+  `record_guid` varchar(50) DEFAULT NULL,
+  `table_name` varchar(150) NOT NULL,
+  `operation` enum('INSERT','UPDATE','DELETE') NOT NULL,
+  `old_data` json DEFAULT NULL,
+  `new_data` json DEFAULT NULL,
+  `changed_by` varchar(45) DEFAULT NULL,
+  `change_timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`guid`),
+  KEY `idx_table_name` (`table_name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- Kiosk Role
 INSERT INTO `idms`.`role` (`guid`, `department`, `position`, `code`, `description`) VALUES ('2a8999bc25cc4c2fa7d3dd6871ab638c', 'KIOSK', 'IN_GATE', 'KIOSK_IN_GATE', 'Kiosk In Gate Role');
@@ -20,6 +22,7 @@ INSERT INTO `idms`.`functions` (`guid`, `module`, `submodule`, `action`, `code`)
 -- Kiosk role functions
 INSERT INTO `idms`.`role_functions` (`guid`, `functions_guid`, `role_guid`) VALUES ('399ee14ed75e41cb93e9ef0506a8f03b', 'f6bfbb4982334f9193314ba288bcddad', '2a8999bc25cc4c2fa7d3dd6871ab638c');
 INSERT INTO `idms`.`role_functions` (`guid`, `functions_guid`, `role_guid`) VALUES ('980c3153065548749cb7a6a09b883461', '2cba511ed21a47738bd0c613474cc133', '66509c2d3af64a0ebdbe3ac74336aa0a');
+
 
 INSERT INTO `idms`.`code_values` (`guid`, `description`, `code_val_type`, `code_val`) VALUES ('5290', 'Residue Disposal', 'DEPARTMENT', 'RESIDUE_DISPOSAL');
 INSERT INTO `idms`.`code_values` (`guid`, `description`, `code_val_type`, `code_val`) VALUES ('5291', 'Dashboard', 'DEPARTMENT', 'DASHBOARD');
