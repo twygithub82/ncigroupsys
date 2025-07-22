ff0201b0e05d72b1a302268ce7e8342edefedb1b
Wymund <weaiyep2002@gmail.com>
2025-07-23 00:53:02 +0800

    added script update code_val

---
+++ database-script/DB_Migration_20250722_SIT.sql
@@ -0,0 +1,5 @@
+
+UPDATE `idms`.`code_values` SET `sequence` = '1' WHERE (`guid` = '604');
+UPDATE `idms`.`code_values` SET `sequence` = '6' WHERE (`guid` = '102');
+UPDATE `idms`.`code_values` SET `sequence` = '3' WHERE (`guid` = '103');
+UPDATE `idms`.`code_values` SET `sequence` = '2' WHERE (`guid` = '601');
