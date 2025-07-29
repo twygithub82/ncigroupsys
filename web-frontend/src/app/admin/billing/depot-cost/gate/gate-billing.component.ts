cd98e31a918f9fc662a0cc4a03dced7e4b2e0baa
Wymund <weaiyep2002@gmail.com>
2025-07-29 22:32:47 +0800

    remove clear all confirmation dialog

--- web-frontend/src/app/admin/billing/depot-cost/gate/gate-billing.component.ts
+++ web-frontend/src/app/admin/billing/depot-cost/gate/gate-billing.component.ts
@@ -644,18 +644,20 @@ export class GateBillingComponent extends UnsubscribeOnDestroyAdapter implements
     } else {
       tempDirection = 'ltr';
     }
-    const dialogRef = this.dialog.open(ConfirmationDialogComponent, {
-      data: {
-        headerText: this.translatedLangText.CONFIRM_CLEAR_ALL,
-        action: 'new',
-      },
-      direction: tempDirection
-    });
-    this.subs.sink = dialogRef.afterClosed().subscribe((result) => {
-      if (result.action === 'confirmed') {
-        this.resetForm();
-      }
-    });
+    this.resetForm();
+    this.search();
+    // const dialogRef = this.dialog.open(ConfirmationDialogComponent, {
+    //   data: {
+    //     headerText: this.translatedLangText.CONFIRM_CLEAR_ALL,
+    //     action: 'new',
+    //   },
+    //   direction: tempDirection
+    // });
+    // this.subs.sink = dialogRef.afterClosed().subscribe((result) => {
+    //   if (result.action === 'confirmed') {
+    //     this.resetForm();
+    //   }
+    // });
   }
 
   resetForm() {
