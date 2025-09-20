f714b61d002fe0ea04d72d31a48818e6d0c5f068
Wymund <weaiyep2002@gmail.com>
2025-09-20 15:26:56 +0800

    change tank info layout to same as cleaning bay overview

--- web-frontend/src/app/admin/steam/bay-overview/bay-overview.component.ts
+++ web-frontend/src/app/admin/steam/bay-overview/bay-overview.component.ts
@@ -807,7 +807,7 @@ export class BayOverviewComponent extends UnsubscribeOnDestroyAdapter implements
       tempDirection = 'ltr';
     }
     const dialogRef = this.dialog.open(TankInfoFormDialogComponent, {
-      width: '85vw',
+      width: '55vw',
       disableClose: true,
       data: {
         selectedItem: team.jobOrderItem?.storing_order_tank!,
