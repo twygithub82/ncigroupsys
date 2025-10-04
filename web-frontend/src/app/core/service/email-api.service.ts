2e86998f65760970e56994197f2236531f75ec50
danielwongsh <74439785+danielwongsh@users.noreply.github.com>
2025-10-04 13:11:54 +0800

    add send emails api

---
+++ web-frontend/src/app/core/service/email-api.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
+import { ApiService } from "./api.service";
+import { api_endpoints } from "app/api-endpoints";
+
+@Injectable({ providedIn: 'root' })
+export class EmailApiService {
+  constructor(private api: ApiService) { }
+
+  email(tankNo: string, groupId: string, recipient: string[]): Observable<any> {
+    const endpoint = api_endpoints.email;
+    return this.api.post<any>(endpoint, { tankNo, groupId, recipient });
+  }
+}
