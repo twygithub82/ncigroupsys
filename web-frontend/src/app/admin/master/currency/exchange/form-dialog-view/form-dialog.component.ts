b0268c71451e73658883d7b150fefceca2229673
bh <bhang2k@gmail.com>
2026-01-11 21:09:33 +0800

    Init version - Currency Exchange

---
+++ web-frontend/src/app/admin/master/currency/exchange/form-dialog-view/form-dialog.component.ts
@@ -0,0 +1,344 @@
+import { CommonModule } from '@angular/common';
+import { Component, Inject } from '@angular/core';
+import { FormsModule, ReactiveFormsModule, UntypedFormBuilder, UntypedFormControl, UntypedFormGroup } from '@angular/forms';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatNativeDateModule, MatOptionModule } from '@angular/material/core';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MAT_DIALOG_DATA, MatDialogContent, MatDialogRef } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { MatSortModule } from '@angular/material/sort';
+import { MatTableModule } from '@angular/material/table';
+import { MatTabsModule } from '@angular/material/tabs';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
+import { Apollo } from 'apollo-angular';
+import { StoringOrderTankItem } from 'app/data-sources/storing-order-tank';
+import { TariffCleaningItem } from 'app/data-sources/tariff-cleaning';
+import { Utility } from 'app/utilities/utility';
+import { provideNgxMask } from 'ngx-mask';
+//import {CleaningCategoryDS,CleaningCategoryItem} from 'app/data-sources/cleaning-category';
+import { TariffDepotItem } from 'app/data-sources/tariff-depot';
+import { PreventNonNumericDirective } from 'app/directive/prevent-non-numeric.directive';
+export interface DialogData {
+  action?: string;
+  selectedValue?: number;
+  // item: StoringOrderTankItem;
+  langText?: any;
+  selectedItem: TariffDepotItem;
+  // populateData?: any;
+  // index: number;
+  // sotExistedList?: StoringOrderTankItem[]
+}
+
+
+
+@Component({
+  selector: 'app-tariff-depot-form-dialog',
+  templateUrl: './form-dialog.component.html',
+  styleUrls: ['./form-dialog.component.scss'],
+  providers: [provideNgxMask()],
+  standalone: true,
+  imports: [
+    MatButtonModule,
+    MatIconModule,
+    MatDialogContent,
+    FormsModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatRadioModule,
+    MatDatepickerModule,
+    MatSelectModule,
+    MatOptionModule,
+    MatNativeDateModule,
+    TranslateModule,
+    MatCheckboxModule,
+    MatAutocompleteModule,
+    CommonModule,
+    MatTabsModule,
+    MatTableModule,
+    MatSortModule,
+    PreventNonNumericDirective
+  ],
+})
+export class FormDialogComponent_View {
+  displayedColumns = [
+    //  'select',
+    // 'img',
+    'fName',
+    'lName',
+    'email',
+    // 'gender',
+    // 'bDate',
+    // 'mobile',
+    // 'actions',
+  ];
+
+  action: string;
+  index?: number;
+  dialogTitle?: string;
+
+
+  storingOrderTank?: StoringOrderTankItem;
+  sotExistedList?: StoringOrderTankItem[];
+  last_cargoList?: TariffCleaningItem[];
+  startDate = new Date();
+  pcForm: UntypedFormGroup;
+  lastCargoControl = new UntypedFormControl();
+  isMobile:boolean=false;
+  //custCompClnCatDS :CustomerCompanyCleaningCategoryDS;
+  //catDS :CleaningCategoryDS;
+  translatedLangText: any = {};
+  langText = {
+    NEW: 'COMMON-FORM.NEW',
+    EDIT: 'COMMON-FORM.EDIT',
+    HEADER: 'COMMON-FORM.CARGO-DETAILS',
+    HEADER_OTHER: 'COMMON-FORM.CARGO-OTHER-DETAILS',
+    CUSTOMER_CODE: 'COMMON-FORM.CUSTOMER-CODE',
+    CUSTOMER_COMPANY_NAME: 'COMMON-FORM.COMPANY-NAME',
+    UNIT_TYPE: 'COMMON-FORM.UNIT-TYPE',
+    REMARKS: 'COMMON-FORM.REMARKS',
+    SO_REQUIRED: 'COMMON-FORM.IS-REQUIRED',
+    STATUS: 'COMMON-FORM.STATUS',
+    UPDATE: 'COMMON-FORM.UPDATE',
+    CANCEL: 'COMMON-FORM.CANCEL',
+    NO_RESULT: 'COMMON-FORM.NO-RESULT',
+    SAVE_SUCCESS: 'COMMON-FORM.ACTION-SUCCESS',
+    BACK: 'COMMON-FORM.BACK',
+    SEARCH: 'COMMON-FORM.SEARCH',
+    SAVE_AND_SUBMIT: 'COMMON-FORM.SAVE-AND-SUBMIT',
+    ARE_YOU_SURE_DELETE: 'COMMON-FORM.ARE-YOU-SURE-DELETE',
+    DELETE: 'COMMON-FORM.DELETE',
+    CLOSE: 'COMMON-FORM.CLOSE',
+    INVALID: 'COMMON-FORM.INVALID',
+    EXISTED: 'COMMON-FORM.EXISTED',
+    DUPLICATE: 'COMMON-FORM.DUPLICATE',
+    SELECT_ATLEAST_ONE: 'COMMON-FORM.SELECT-ATLEAST-ONE',
+    ADD_ATLEAST_ONE: 'COMMON-FORM.ADD-ATLEAST-ONE',
+    ROLLBACK_STATUS: 'COMMON-FORM.ROLLBACK-STATUS',
+    CANCELED_SUCCESS: 'COMMON-FORM.ACTION-SUCCESS',
+    ARE_YOU_SURE_CANCEL: 'COMMON-FORM.ARE-YOU-SURE-CANCEL',
+    ARE_YOU_SURE_ROLLBACK: 'COMMON-FORM.ARE-YOU-SURE-ROLLBACK',
+    BULK: 'COMMON-FORM.BULK',
+    CONFIRM: 'COMMON-FORM.CONFIRM',
+    UNDO: 'COMMON-FORM.UNDO',
+    CARGO_NAME: 'COMMON-FORM.CARGO-NAME',
+    CARGO_ALIAS: 'COMMON-FORM.CARGO-ALIAS',
+    CARGO_DESCRIPTION: 'COMMON-FORM.CARGO-DESCRIPTION',
+    CARGO_CLASS: 'COMMON-FORM.CARGO-CLASS',
+    CARGO_CLASS_SELECT: 'COMMON-FORM.CARGO-CLASS-SELECT',
+    CARGO_UN_NO: 'COMMON-FORM.CARGO-UN-NO',
+    CARGO_METHOD: 'COMMON-FORM.CARGO-METHOD',
+    CARGO_CATEGORY: 'COMMON-FORM.CARGO-CATEGORY',
+    CARGO_FLASH_POINT: 'COMMON-FORM.CARGO-FLASH-POINT',
+    CARGO_COST: 'COMMON-FORM.CARGO-COST',
+    CARGO_HAZARD_LEVEL: 'COMMON-FORM.CARGO-HAZARD-LEVEL',
+    CARGO_BAN_TYPE: 'COMMON-FORM.CARGO-BAN-TYPE',
+    CARGO_NATURE: 'COMMON-FORM.CARGO-NATURE',
+    CARGO_REQUIRED: 'COMMON-FORM.IS-REQUIRED',
+    CARGO_NOTE: 'COMMON-FORM.CARGO-NOTE',
+    PACKAGE_MIN_COST: 'COMMON-FORM.PACKAGE-MIN-COST',
+    PACKAGE_MAX_COST: 'COMMON-FORM.PACKAGE-MAX-COST',
+    PACKAGE_DETAIL: 'COMMON-FORM.PACKAGE-DETAIL',
+    PACKAGE_CLEANING_ADJUSTED_COST: "COMMON-FORM.PACKAGE-CLEANING-ADJUST-COST",
+    PROFILE_NAME: 'COMMON-FORM.PROFILE-NAME',
+    VIEW: 'COMMON-FORM.VIEW',
+    DEPOT_PROFILE: 'COMMON-FORM.DEPOT-PROFILE',
+    DESCRIPTION: 'COMMON-FORM.DESCRIPTION',
+    PREINSPECTION_COST: "COMMON-FORM.PREINSPECTION-COST",
+    LOLO_COST: "COMMON-FORM.LOLO-COST",
+    STORAGE_COST: "COMMON-FORM.STORAGE-COST",
+    FREE_STORAGE: "COMMON-FORM.FREE-STORAGE",
+    LAST_UPDATED_DT: 'COMMON-FORM.LAST-UPDATED'
+
+  };
+  unit_type_control = new UntypedFormControl();
+
+  selectedItem: TariffDepotItem;
+
+  constructor(
+    public dialogRef: MatDialogRef<FormDialogComponent_View>,
+    @Inject(MAT_DIALOG_DATA) public data: DialogData,
+    private fb: UntypedFormBuilder,
+    private apollo: Apollo,
+    private translate: TranslateService,
+    private snackBar: MatSnackBar,
+  ) {
+    // Set the defaults
+    this.selectedItem = data.selectedItem;
+
+    this.pcForm = this.createTariffDepot();
+    this.pcForm.get('last_updated')?.setValue(this.displayLastUpdated(this.selectedItem));
+    this.action = data.action!;
+    this.translateLangText();
+    this.isMobile=Utility.isMobile();
+  }
+
+  createTariffDepot(): UntypedFormGroup {
+    return this.fb.group({
+      selectedItem: this.selectedItem,
+      action: this.action,
+      name: this.selectedItem.profile_name,
+      description: this.selectedItem.description,
+      preinspection_cost: this.selectedItem.preinspection_cost,
+      lolo_cost: this.selectedItem.lolo_cost,
+      storage_cost: this.selectedItem.storage_cost,
+      free_storage: this.selectedItem.free_storage,
+      unit_types: this.unit_type_control,
+      last_updated: ['']
+    });
+  }
+
+  GetButtonCaption() {
+    if (this.pcForm!.value['action'] == "view") {
+      return this.translatedLangText.CLOSE;
+    } else {
+      return this.translatedLangText.CANCEL;
+    }
+  }
+
+  GetTitle() {
+    return this.translatedLangText.VIEW + " " + this.translatedLangText.DEPOT_PROFILE;
+  }
+
+  translateLangText() {
+    Utility.translateAllLangText(this.translate, this.langText).subscribe((translations: any) => {
+      this.translatedLangText = translations;
+    });
+  }
+
+
+  canEdit() {
+    return this.pcForm!.value['action'] == "new";
+  }
+
+  handleSaveSuccess(count: any) {
+    if ((count ?? 0) > 0) {
+
+      console.log('valid');
+      this.dialogRef.close(count);
+    }
+  }
+
+  save() {
+    if (!this.pcForm?.valid) return;
+    const where: any = {};
+    if (this.pcForm!.value['name']) {
+      where.name = { contains: this.pcForm!.value['name'] };
+    }
+
+    // this.catDS.search(where).subscribe(p=>{
+    //    if(p.length==0)
+    //    {
+    //     if (this.selectedItem.guid) {
+
+    //       this.catDS.updateCleaningCategory(cc).subscribe(result => {
+    //         console.log(result)
+    //         this.handleSaveSuccess(result?.data?.updateCleaningCategory);
+    //         });
+
+    //     }
+    //     else
+    //     {
+    //      this.catDS.addCleaningCategory(cc).subscribe(result => {
+    //       console.log(result)
+    //       this.handleSaveSuccess(result?.data?.addCleaningCategory);
+    //       });
+    //     }
+
+    //    }
+    //    else
+    //    {
+    //       var allowUpdate=true;
+    //       for (let i = 0; i < p.length; i++) {
+    //         if (p[i].guid != this.selectedItem.guid) {
+    //           allowUpdate = false;
+    //           break;  // Exit the loop
+    //         }
+    //       }
+    //       if(allowUpdate)
+    //       {
+
+    //         if (this.selectedItem.guid) {
+
+    //           this.catDS.updateCleaningCategory(cc).subscribe(result => {
+    //             console.log(result)
+    //             this.handleSaveSuccess(result?.data?.updateCleaningCategory);
+    //             });
+
+    //         }
+    //       }
+    //       else
+    //       {
+    //          this.pcForm?.get('name')?.setErrors({ existed: true });
+    //       }
+    //    }
+    // });
+    // let pc_guids:string[] = this.selectedItems
+    // .map(cc => cc.guid)
+    // .filter((guid): guid is string => guid !== undefined);
+
+    // var adjusted_price = Number(this.pcForm!.value["adjusted_cost"]);
+    // var remarks = this.pcForm!.value["remarks"];
+
+    // this.custCompClnCatDS.updatePackageCleanings(pc_guids,remarks,adjusted_price).subscribe(result => {
+    //   console.log(result)
+    //   if(result.data.updatePackageCleans>0)
+    //   {
+    //       //this.handleSaveSuccess(result?.data?.updateTariffClean);
+    //       const returnDialog: DialogData = {
+    //         selectedValue:result.data.updatePackageCleans,
+    //         selectedItems:[]
+    //       }
+    //       console.log('valid');
+    //       this.dialogRef.close(returnDialog);
+    //   }
+    // });
+
+
+
+  }
+
+  displayLastUpdated(r: TariffDepotItem) {
+    var updatedt = r.update_dt;
+    if (updatedt === null) {
+      updatedt = r.create_dt;
+    }
+    const date = new Date(updatedt! * 1000);
+    const day = String(date.getDate()).padStart(2, '0');
+    const month = date.toLocaleString('en-US', { month: 'short' });
+    const year = date.getFullYear();
+
+    // Replace the '/' with '-' to get the required format
+
+
+    return `${day}/${month}/${year}`;
+
+  }
+
+  markFormGroupTouched(formGroup: UntypedFormGroup): void {
+    Object.keys(formGroup.controls).forEach((key) => {
+      const control = formGroup.get(key);
+      if (control instanceof UntypedFormGroup) {
+        this.markFormGroupTouched(control);
+      } else {
+        control!.markAsTouched();
+      }
+    });
+  }
+  onNoClick(): void {
+    this.dialogRef.close();
+  }
+
+  getColumnClasses(baseClasses: string, Padding: boolean = true): string {
+      const centerClass = Padding ? 'px-3' : '';
+      return `${baseClasses} ${centerClass}`.trim();
+    }
+
+}
---
+++ web-frontend/src/app/admin/master/currency/main-currency.component.ts
@@ -0,0 +1,179 @@
+import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { FormsModule, ReactiveFormsModule, UntypedFormBuilder } from '@angular/forms';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatRippleModule } from '@angular/material/core';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatDialog } from '@angular/material/dialog';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { MatSortModule } from '@angular/material/sort';
+import { MatTableModule } from '@angular/material/table';
+import { MatTabChangeEvent, MatTabsModule } from '@angular/material/tabs';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
+import { UnsubscribeOnDestroyAdapter } from '@shared';
+import { BreadcrumbComponent } from '@shared/components/breadcrumb/breadcrumb.component';
+import { Apollo } from 'apollo-angular';
+import { Utility } from 'app/utilities/utility';
+import { CurrencyExchangeComponent } from './exchange/currency-exchange.component';
+import { ModulePackageService } from 'app/services/module-package.service';
+@Component({
+  selector: 'app-tariff-main-clean',
+  standalone: true,
+  templateUrl: './main-currency.component.html',
+  styleUrl: './main-currency.component.scss',
+  imports: [
+    BreadcrumbComponent,
+    MatTooltipModule,
+    MatButtonModule,
+    MatIconModule,
+    MatTableModule,
+    MatSortModule,
+    MatCheckboxModule,
+    MatRippleModule,
+    MatProgressSpinnerModule,
+    MatMenuModule,
+    TranslateModule,
+    MatExpansionModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatDatepickerModule,
+    MatSelectModule,
+    CommonModule,
+    ReactiveFormsModule,
+    FormsModule,
+    MatAutocompleteModule,
+    MatDividerModule,
+    MatCardModule,
+    MatTabsModule,
+    CurrencyExchangeComponent
+
+  ]
+})
+export class MainCurrencyComponent extends UnsubscribeOnDestroyAdapter implements OnInit {
+  pageTitle = 'MENUITEMS.MASTER.LIST.CURRENCY'
+  breadcrumsMiddleList = [
+    { text: 'MENUITEMS.MASTER.TEXT', route: '/admin/master/currency' }
+  ]
+
+  translatedLangText: any = {};
+  langText = {
+    STATUS: 'COMMON-FORM.STATUS',
+    SO_NO: 'COMMON-FORM.SO-NO',
+    CUSTOMER: 'COMMON-FORM.CUSTOMER',
+    EIR_DATE: 'COMMON-FORM.EIR-DATE',
+    EIR_NO: 'COMMON-FORM.EIR-NO',
+    PART_NAME: 'COMMON-FORM.PART-NAME',
+    LAST_CARGO: 'COMMON-FORM.LAST-CARGO',
+    TANK_NO: 'COMMON-FORM.TANK-NO',
+    JOB_NO: 'COMMON-FORM.JOB-NO',
+    PURPOSE: 'COMMON-FORM.PURPOSE',
+    ETA_DATE: 'COMMON-FORM.ETA-DATE',
+    NO_RESULT: 'COMMON-FORM.NO-RESULT',
+    ARE_YOU_SURE_CANCEL: 'COMMON-FORM.ARE-YOU-SURE-CANCEL',
+    CANCEL: 'COMMON-FORM.CANCEL',
+    CLOSE: 'COMMON-FORM.CLOSE',
+    TO_BE_CANCELED: 'COMMON-FORM.TO-BE-CANCELED',
+    CANCELED_SUCCESS: 'COMMON-FORM.ACTION-SUCCESS',
+    ADD: 'COMMON-FORM.ADD',
+    REFRESH: 'COMMON-FORM.REFRESH',
+    EXPORT: 'COMMON-FORM.EXPORT',
+    REMARKS: 'COMMON-FORM.REMARKS',
+    SO_REQUIRED: 'COMMON-FORM.IS-REQUIRED',
+    INVALID_SELECTION: 'COMMON-FORM.INVALID-SELECTION',
+    ACCEPTED: 'COMMON-FORM.ACCEPTED',
+    WAITING: 'COMMON-FORM.WAITING',
+    CANCELED: 'COMMON-FORM.CANCELED',
+    TANKS: 'COMMON-FORM.TANKS',
+    CONFIRM: 'COMMON-FORM.CONFIRM',
+    BILL_COMPLETED: 'COMMON-FORM.BILL-COMPLETED',
+    REPAIR_JOB_NO: 'COMMON-FORM.REPAIR-JOB-NO',
+    REPAIR_TYPE: 'COMMON-FORM.REPAIR-TYPE',
+    ESTIMATE_DATE: 'COMMON-FORM.ESTIMATE-DATE',
+    APPROVAL_DATE: 'COMMON-FORM.APPROVAL-DATE',
+    ESTIMATE_STATUS: 'COMMON-FORM.ESTIMATE-STATUS',
+    CURRENT_STATUS: 'COMMON-FORM.CURRENT-STATUS',
+    ESTIMATE_NO: 'COMMON-FORM.ESTIMATE-NO',
+    NET_COST: 'COMMON-FORM.NET-COST',
+    CONFIRM_CLEAR_ALL: 'COMMON-FORM.CONFIRM-CLEAR-ALL',
+    CLEAR_ALL: 'COMMON-FORM.CLEAR-ALL',
+    AMEND: 'COMMON-FORM.AMEND',
+    CHANGE_REQUEST: 'COMMON-FORM.CHANGE-REQUEST',
+    REPAIR_EST_TAB_TITLE: 'COMMON-FORM.JOB-ALLOCATION',
+    JOB_ORDER_TAB_TITLE: 'COMMON-FORM.JOBS',
+    JOB_ORDER_NO: 'COMMON-FORM.JOB-ORDER-NO',
+    METHOD: "COMMON-FORM.METHOD",
+    QC: 'COMMON-FORM.QC',
+    BAY_OVERVIEW: "COMMON-FORM.BAY-OVERVIEW",
+    CLEANING: "COMMON-FORM.CLEANING",
+    CLEANING_BILLING: "MENUITEMS.BILLING.LIST.CLEANING-BILL",
+    STEAM_BILLING: "MENUITEMS.BILLING.LIST.STEAM-BILL",
+    RESIDUE_BILLING: "MENUITEMS.BILLING.LIST.RESIDUE-DISPOSAL-BILL",
+    TARIFF_CLEANING: 'MENUITEMS.TARIFF.LIST.TARIFF-CLEANING',
+    TARIFF_BUFFER: 'MENUITEMS.TARIFF.LIST.TARIFF-BUFFER',
+    TARIFF_RESIDUE: 'MENUITEMS.TARIFF.LIST.TARIFF-RESIDUE',
+    TARIFF_LABOUR: 'MENUITEMS.TARIFF.LIST.TARIFF-LABOUR',
+    TARIFF_DEPOT: 'MENUITEMS.TARIFF.LIST.TARIFF-DEPOT',
+    CURRENCY: 'MENUITEMS.MASTER.LIST.CURRENCY',
+  }
+
+  tabConfig = [
+    {
+      label: this.translatedLangText.CURRENCY,
+      component: 'app-currency-exchange',
+      modulePackage: ['starter', 'growth', 'customized'],
+      expectedFunctions: ['MASTER_CURRENCY_EDIT', 'MASTER_CURRENCY_ADD', 'MASTER_CURRENCY_DELETE', 'MASTER_CURRENCY_VIEW'],
+    }
+   
+  ];
+
+  get allowedTabs() {
+    return this.tabConfig.filter(tab => {
+      return this.modulePackageService.hasFunctions(tab.expectedFunctions)
+    });
+  }
+
+  constructor(
+    public httpClient: HttpClient,
+    public dialog: MatDialog,
+    private snackBar: MatSnackBar,
+    private fb: UntypedFormBuilder,
+    private apollo: Apollo,
+    private translate: TranslateService,
+    private modulePackageService: ModulePackageService
+  ) {
+    super();
+    this.translateLangText();
+  }
+
+  ngOnInit() {
+  }
+
+  translateLangText() {
+    Utility.translateAllLangText(this.translate, this.langText).subscribe((translations: any) => {
+      this.translatedLangText = translations;
+    });
+  }
+
+  @ViewChild('currencyExchange') currencyExchange!: CurrencyExchangeComponent;
+  
+  onTabSelected(event: MatTabChangeEvent): void {
+    console.log(`Selected Index: ${event.index}, Tab Label: ${event.tab.textLabel}`);
+    switch (event.index) {
+      case 0:
+        this.currencyExchange?.onTabFocused(); break;
+    }
+  }
+}
