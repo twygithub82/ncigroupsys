d12bde6a0001e364e25166e5a5085cec129ac943
bh <bhang2k@gmail.com>
2026-01-29 16:41:43 +0800

    rectified the tariff repair report

--- web-frontend/src/environments/environment.ts
+++ web-frontend/src/environments/environment.ts
@@ -1,4 +1,4 @@
-export const environment = {
+export const environment1 = {
   production: true,
   title: 'IDMS',
   companyNameShort: 'NCI Global',
@@ -7,6 +7,19 @@ export const environment = {
   fileManagerURL: 'https://tlx-filemanagement-app-uat.azurewebsites.net',
   graphQLUrl: 'https://tlx-idms-gateway-uat.azurewebsites.net/graphql',
   graphqlWsUrl: 'wss://tlx-idms-notification-uat.greenplant-68cf0a82.southeastasia.azurecontainerapps.io/graphql',
+  topicSubscribe: '/idms/all/*'
+};
+
+export const environment= {
+  production: false,
+  title: 'SIT IDMS',
+  companyNameShort: 'NCI Global',
+  companyName: 'NCI GLOBAL PTE LTD',
+  apiUrl: 'https://tlx-idms-userlogin.azurewebsites.net',
+  fileManagerURL: 'https://tlx-filemanagement-app-b6aga4fcfwhbggd7.southeastasia-01.azurewebsites.net',
+  graphQLUrl: 'https://tlx-idms-gateway.azurewebsites.net/graphql',
+  //  graphQLUrl: 'https://tlx-idms-gateway-uat.azurewebsites.net/graphql',
+  graphqlWsUrl: 'wss://tlx-idms-notification.happyocean-dddaac7a.southeastasia.azurecontainerapps.io/graphql',
   topicSubscribe: '/idms/all/*'
 };
 
