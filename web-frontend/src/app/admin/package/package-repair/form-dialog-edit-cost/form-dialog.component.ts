20ad45f9b2922d1c2c988a800c7483d755ab69e8
Wymund <weaiyep2002@gmail.com>
2026-04-01 23:05:15 +0800

    resolve compile error in UAT

--- web-frontend/src/app/admin/package/package-repair/form-dialog-edit-cost/form-dialog.component.ts
+++ web-frontend/src/app/admin/package/package-repair/form-dialog-edit-cost/form-dialog.component.ts
@@ -778,4 +778,17 @@ export class FormDialogComponent_Edit_Cost extends UnsubscribeOnDestroyAdapter {
       !control?.value
     );
   }
+
+    getColumnClasses(baseClasses: string, Padding: boolean = true): string {
+      const centerClass = Padding ? 'px-3' : '';
+      return `${baseClasses} ${centerClass}`.trim();
+    }
+     getColumnClasses_center(baseClasses: string, isCenter: boolean = true): string {
+      const centerClass = isCenter ? 'justify-content-center' : '';
+      return `${baseClasses} ${centerClass}`.trim();
+    }
+    getColumnClasses_row(baseClasses: string, wholeRow: boolean = true): string {
+      const centerClass = wholeRow ? 'col-xl-12 col-lg-12 ' : 'col-xl-5-75 col-lg-5-75 ';
+      return `${baseClasses} ${centerClass}`.trim();
+    }
 }
