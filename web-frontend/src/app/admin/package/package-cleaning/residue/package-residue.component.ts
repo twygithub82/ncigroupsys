2e16fd8f25a543f2bcf9155040097c6f8e517ec9
bh <bhang2k@gmail.com>
2026-04-21 20:37:00 +0800

    resolve duplicated function issue

--- web-frontend/src/app/admin/package/package-cleaning/residue/package-residue.component.ts
+++ web-frontend/src/app/admin/package/package-cleaning/residue/package-residue.component.ts
@@ -1103,67 +1103,6 @@ export class PackageResidueComponent extends UnsubscribeOnDestroyAdapter
       return `${baseClasses} ${centerClass}`.trim();
     }
 
-  export_excel()
-            {
-              this.isGeneratingReport=true;
-               const where={and:[{ delete_dt: { eq: null } },{customer_company:{ delete_dt: { eq: null } }}]};
-              const order=this.lastOrderBy;
-              this.packResidueDS.SearchAllPackageResidue(where,order).subscribe(res=>{
-                    var prcList:PackageResidueItem[]=res;
-                    this.exportExcelReport(prcList);
-              })
-            //  if(this.packResidueItems) 
-            //  {
-            //   this.isGeneratingReport=true;
-            //   var prcList:PackageResidueItem[]=[];
-            //       this.packResidueItems.forEach((item)=>{
-            //         var itm:any = item;
-            //        const c: PackageResidueItem = {
-            //           ...itm,
-                     
-            //         };
-            //         prcList.push(c);
-            //       });
-            //   this.exportExcelReport(prcList);
-            //  }
-          
-            }
-        
-          exportExcelReport(repData:any) {
-                  
-                     //this.preventDefault(event);
-                      let cut_off_dt = new Date();
-                  
-                  
-                      let tempDirection: Direction;
-                      if (localStorage.getItem('isRtl') === 'true') {
-                        tempDirection = 'rtl';
-                      } else {
-                        tempDirection = 'ltr';
-                      }
-                  
-                      const dialogRef = this.dialog.open(PackageResidueCostExcelComponent, {
-                        width: reportPreviewWindowDimension.portrait_width_rate,
-                        maxWidth: reportPreviewWindowDimension.portrait_maxWidth,
-                        maxHeight: reportPreviewWindowDimension.report_maxHeight,
-                        
-                        data: {
-                          repData: repData
-                        },
-                  
-                        // panelClass: this.eirPdf?.length ? 'no-scroll-dialog' : '',
-                        direction: tempDirection
-                      });
-                  
-                        dialogRef.updatePosition({
-                        top: '-90vh',  // Move far above the screen
-                        left: '0px'  // Move far to the left of the screen
-                      });
-                  
-                      this.subs.sink = dialogRef.afterClosed().subscribe((result) => {
-                        this.isGeneratingReport = false;
-                      });
-              
-                }
+ 
 }
 
