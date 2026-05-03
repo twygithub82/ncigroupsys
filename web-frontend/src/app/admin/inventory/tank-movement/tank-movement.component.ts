a7123e8176763745f355580679447e056429d53f
Wymund <weaiyep2002@gmail.com>
2026-05-03 16:17:48 +0800

    fix tank movement clear all not working

--- web-frontend/src/app/admin/inventory/tank-movement/tank-movement.component.ts
+++ web-frontend/src/app/admin/inventory/tank-movement/tank-movement.component.ts
@@ -590,11 +590,14 @@ export class TankMovementComponent extends UnsubscribeOnDestroyAdapter implement
       tank_no: '',
       job_no: '',
       purpose: '',
+      yard_cv: '',
       tank_status_cv: '',
       eir_status_cv: '',
       last_cargo: ''
     });
     this.customerCodeControl.reset('');
+    this.selectedNames = [];
+    this.cargo_removeAllSelected();
   }
 
   @ViewChild('nameInput', { static: true })
