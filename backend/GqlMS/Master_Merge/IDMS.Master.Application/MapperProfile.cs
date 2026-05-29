20b90ac183f72cc38957009e1146ac17fe94b6b9
Wymund <weaiyep2002@gmail.com>
2026-05-29 11:30:03 +0800

    fixed Master module no mapper profile

---
+++ backend/GqlMS/Master_Merge/IDMS.Master.Application/MapperProfile.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using IDMS.Customer.GqlTypes.LocalModel;
+using IDMS.Models.Inventory;
+using IDMS.Models.Master;
+using IDMS.Models.Shared;
+
+namespace IDMS.Master.Application
+{
+    public class MapperProfile: Profile
+    {
+        public MapperProfile()
+        {
+            //CreateMap<InGateSurveyRequest, in_gate_survey>()
+            //.ForMember(dest => dest.guid, opt => opt.Ignore());
+
+            // Add this line to bridge the gap
+            CreateMap<CustomerRequest, customer_company>();
+        }
+    }
+}
