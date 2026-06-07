4a77cc96f261d326d6063af981d74a8b46ae47df
danielwongsh <74439785+danielwongsh@users.noreply.github.com>
2026-06-07 13:17:33 +0800

    add release order details route and path accordingly

--- web-frontend/src/app/admin/inventory/release-order/release-order.routes.ts
+++ web-frontend/src/app/admin/inventory/release-order/release-order.routes.ts
@@ -1,12 +1,25 @@
 import { Routes } from "@angular/router";
 import { AuthGuard } from "@core/guard/auth.guard";
 import { ReleaseOrderComponent } from "./release-order.component";
+import { ReleaseOrderDetailsComponent } from "../release-order-details/release-order-details.component";
 
 export const RELEASE_ORDER_ROUTES: Routes = [
   {
-      path: "",
-      component: ReleaseOrderComponent,
-      canActivate: [AuthGuard],
-      data: { expectedFunctions: ['INVENTORY_RELEASE_ORDER_VIEW'] }
-    },
+    path: "",
+    component: ReleaseOrderComponent,
+    canActivate: [AuthGuard],
+    data: { expectedFunctions: ['INVENTORY_RELEASE_ORDER_VIEW', 'INVENTORY_RELEASE_ORDER_EDIT', 'INVENTORY_RELEASE_ORDER_DELETE'] }
+  },
+  {
+    path: "new",
+    component: ReleaseOrderDetailsComponent,
+    canActivate: [AuthGuard],
+    data: { expectedFunctions: ['INVENTORY_RELEASE_ORDER_VIEW', 'INVENTORY_RELEASE_ORDER_EDIT', 'INVENTORY_RELEASE_ORDER_DELETE', 'INVENTORY_RELEASE_ORDER_ADD'] }
+  },
+  {
+    path: "edit/:id",
+    component: ReleaseOrderDetailsComponent,
+    canActivate: [AuthGuard],
+    data: { expectedFunctions: ['INVENTORY_RELEASE_ORDER_VIEW', 'INVENTORY_RELEASE_ORDER_EDIT', 'INVENTORY_RELEASE_ORDER_DELETE'] }
+  },
 ];
