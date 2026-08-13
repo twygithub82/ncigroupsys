b9cb75656f804a04f2eeac3424982d65e4f9fb73
danielwongsh <74439785+danielwongsh@users.noreply.github.com>
2026-08-13 23:05:13 +0800

    fix not showing na date for residue

--- web-frontend/src/app/data-sources/residue.ts
+++ web-frontend/src/app/data-sources/residue.ts
@@ -17,6 +17,7 @@ export class ResidueGO {
   public bill_to_guid?: string;
   public complete_by?: string;
   public complete_dt?: number;
+  public na_dt?: number;
   public guid?: string;
   public job_no?: string;
   public remarks?: string;
@@ -44,6 +45,7 @@ export class ResidueGO {
     this.approve_dt = item.approve_dt;
     this.complete_by = item.complete_by;
     this.complete_dt = item.complete_dt;
+    this.na_dt = item.na_dt;
     this.status_cv = item.status_cv;
     this.remarks = item.remarks;
     this.bill_to_guid = item.bill_to_guid;
@@ -564,6 +566,7 @@ export const GET_RESIDUE_FOR_MOVEMENT = gql`
         update_dt
         customer_billing_guid
         owner_billing_guid
+        na_dt
         residue_part(where: {delete_dt:  {eq: null}}) {
           action
           approve_part
@@ -1183,8 +1186,10 @@ export class ResidueDS extends BaseDataSource<ResidueItem> {
       return undefined;
     }
 
-    const beginDate = this.getResidueBeginDate(residue);
-    const completeDate = this.getResidueCompleteDate(residue);
+    const actionedResidue = residue.filter(item => item.status_cv !== 'NO_ACTION');
+
+    const beginDate = this.getResidueBeginDate(actionedResidue);
+    const completeDate = this.getResidueCompleteDate(actionedResidue);
 
     if (!beginDate || !completeDate) {
       return undefined;
